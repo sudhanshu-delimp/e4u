9fa0c6f1c13c80373456a1e7935799e8415d2150
Aashish <aashish@delimp.com>
2025-05-16 18:33:57 +0530

    Twitter icon change

--- resources/views/web/pages/abbreviations.blade.php
+++ resources/views/web/pages/abbreviations.blade.php
@@ -795,7 +795,7 @@ Creator's Content, including by subscription and payments made by a User to view
 
                                     <div class=" wi-icon-box">
                                         <div class="mr-3 wi-icon-icon">
-                                        <img src="{{ asset('assets/img/Twitter.png') }}" alt="" class=" wi-icon">
+                                        <img src="{{ asset('assets/img/twitter-custom.png') }}" alt="" class=" wi-icon">
                                         </div>
                                         <div class="wi-icon-content-box">
                                             <p class="wi-icon-content">means the platform known as Twitter</p>
