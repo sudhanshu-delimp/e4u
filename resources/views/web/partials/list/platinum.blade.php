49a12053667144ea30616f313b8cfe79bc5c000b
Sudhanshu Bisht <sudhanshu@delimp.com>
2025-06-03 19:18:20 +0530

    sudhanshu, 4053899 implement timezone with BRB

--- resources/views/web/partials/list/platinum.blade.php
+++ resources/views/web/partials/list/platinum.blade.php
@@ -10,7 +10,7 @@
                             @if($escort->latestActiveBrb)
                             <div class="brb--content">
                                 <div class="brb--wrappr">
-                                    <span class="brb-text">BRB</span> at <span class="brb-time">{{date('h:i A',strtotime($escort->latestActiveBrb->brb_time))}}</span> <span class="brb-date">{{date('d-m-Y',strtotime($escort->latestActiveBrb->brb_time))}}</span>
+                                    <span class="brb-text">BRB</span> at <span class="brb-time">{{date('h:i A',strtotime($escort->latestActiveBrb->selected_time))}}</span> <span class="brb-date">{{date('d-m-Y',strtotime($escort->latestActiveBrb->selected_time))}}</span>
                                 </div>
                             </div>
                             @endif
