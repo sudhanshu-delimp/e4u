bf8c702d96c594c6d713797f2554d554e194b39b
Sudhanshu Bisht <sudhanshu@delimp.com>
2025-06-05 13:25:17 +0530

    sudhanshu, 4053899 readjust code in BRB controller

--- app/Http/Controllers/EscortBrbController.php
+++ app/Http/Controllers/EscortBrbController.php
@@ -4,37 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Carbon\Carbon;
-use App\Models\Escort;
 use App\Models\EscortBrb;
-use App\Models\Timezone;
 
 class EscortBrbController extends Controller
 {
-    function add(Request $request)
-    {
-        $defaultTimezone = date_default_timezone_get();
-        $timezoneId = "";
+    function add(Request $request) {
         $newBrb = new EscortBrb;
-        $profileId =  $request->profile_id;
-        $newBrb->profile_id = $profileId;
-       
-        $newBrb->brb_time = date('Y-m-d H:i:s', strtotime($request->brb_date . ' ' . $request->brb_time));
-        $brbtime = date('d-m-Y h:i A', strtotime($request->brb_date . ' ' . $request->brb_time));
-        /*$escort = Escort::select(['id', 'state_id', 'profile_name'])->where('id', $profileId)->first();
-        if ($escort) {
-            $stateId = $escort->state_id;
-            $timezonert = Timezone::select(['id', 'state_id', 'timezone_id'])->where('state_id', $stateId)->first();
-            if ($timezonert) {
-                $timezoneId = $timezonert->timezone_id;
-                $dt = Carbon::parse($newBrb->brb_time ); // or 'Australia/Brisbane', etc.
-                $calcuttaTime = $dt->setTimezone($timezoneId);
-                $newBrb->brb_time = $calcuttaTime;
-            }
-        }*/
+        $newBrb->profile_id = $request->profile_id;
         $newBrb->date_set = date('Y-m-d');
-
+        
+        $brbtime = date('d-m-Y h:i A', strtotime($request->brb_date.' '.$request->brb_time));
         $newBrb->brb_note = $request->brb_note;
-        if ($newBrb->save()) {
+        $escortDetail = getEscortDetail($request->profile_id);
+        $profileTimezone = config("escorts.profile.states.$escortDetail[state_id].cities.$escortDetail[city_id].timeZone");
+        
+        $localDateTime = Carbon::createFromFormat('Y-m-d H:i', "$request->brb_date $request->brb_time", $profileTimezone);
+        $expiresAtUtc = $localDateTime->copy()->setTimezone('UTC');
+        
+        $newBrb->selected_time = date('Y-m-d H:i', strtotime($request->brb_date.' '.$request->brb_time));
+        $newBrb->brb_time = $expiresAtUtc;
+        if($newBrb->save()) {
             $response = [
                 'success' => true,
                 'brbtime' => $brbtime,
@@ -51,12 +40,11 @@ class EscortBrbController extends Controller
     }
 
 
-    function inactive($id)
-    {
+    function inactive($id) {
         $newBrb = new EscortBrb;
         $newBrb = $newBrb::find($id);
         $newBrb->active = 'N';
-        if ($newBrb->save()) {
+        if($newBrb->save()) {
             $response = [
                 'success' => true,
                 'message' => 'BRB disabled'
