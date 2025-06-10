f44ff6855587e356af1668804aea2b6e875aa317
Mithilesh Kumar <mithilesh@delimp.com>
2025-06-10 15:15:25 +0530

    4081377: create migration file for contactus form

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -126,8 +126,47 @@ if (!function_exists('getEscortDetail')) {
  */
 if (!function_exists('getCountryList')) {
     function getCountryList()
-        {
-            return Country::select(['id', 'name', 'status'])->pluck('name', 'id');
-           
+    {
+        return Country::select(['id', 'name', 'status'])->pluck('name', 'id');
+    }
+}
+
+/**
+ * Create a "Random" Strin
+ *
+ * @param string  type of random string.  basic, alpha, alnum, numeric, nozero, unique, md5, encrypt and sha1
+ * @param int number of characters
+ * @return string
+ */
+if (!function_exists('random_string')) {
+    function random_string($type = 'nozero', $len = 8)
+    {
+        switch ($type) {
+            case 'basic':
+                return mt_rand();
+            case 'alnum':
+            case 'numeric':
+            case 'nozero':
+            case 'alpha':
+                switch ($type) {
+                    case 'alpha':
+                        $pool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                        break;
+                    case 'alnum':
+                        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                        break;
+                    case 'numeric':
+                        $pool = '0123456789';
+                        break;
+                    case 'nozero':
+                        $pool = '123456789';
+                        break;
+                }
+                return substr(str_shuffle(str_repeat($pool, ceil($len / strlen($pool)))), 0, $len);
+            case 'md5':
+                return md5(uniqid(mt_rand()));
+            case 'sha1':
+                return sha1(uniqid(mt_rand(), TRUE));
         }
     }
+}
---
+++ database/migrations/2025_06_10_181765_create_contactus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contactus', function (Blueprint $table) {
+            $table->id();
+            $table->string('ref_number')->index()->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('email')->index()->nullable();
+            $table->string('comments')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contactus');
+    }
+}
