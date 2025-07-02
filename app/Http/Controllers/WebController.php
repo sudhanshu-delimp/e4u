ddf528b62ca6e2f7ffc775d4e89b0b05cb799361
jitender <jitender@delimp.com>
2025-07-02 18:04:57 +0530

    fix brb notes bug

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -1167,7 +1167,11 @@ class WebController extends Controller
         // dd($lp, $dp); lp-67 dp-33
 
         $brb = new EscortBrb();
-        $brb = $brb->where('profile_id', $id)->where('brb_time', '>', date('Y-m-d H:i:s'))->where('active', 'Y')->orderBy('brb_time', 'desc')->first();
+        if(request()->get('brb')){
+            $brb = $brb->where('id', request()->get('brb'))->first();
+        }
+        
+        // $brb = $brb->where('profile_id', $id)->where('brb_time', '>', date('Y-m-d H:i:s'))->where('active', 'Y')->orderBy('brb_time', 'desc')->first();
        
         if($brb) {
             $brb = $brb->toArray(); 
