b161a77c3ea391ef1ea4f93fe51f83cddbc04314
shiv-delimp <shivendra@delimp.com>
2025-07-08 13:41:08 +0530

    working on this task escort console 4243184

--- resources/views/escort/dashboard/Annalytics/social-media.blade.php
+++ resources/views/escort/dashboard/Annalytics/social-media.blade.php
@@ -6,10 +6,10 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/app/vendor/file-upload/css/pintura.min.css') }}">
 
 <style type="text/css">
-    .parsley-errors-list {
-        list-style: none;
-        color: rgb(248, 0, 0)
+    #socialMediaTable td,th{
+        vertical-align: middle;
     }
+
 </style>
 @endsection
 @section('content')
@@ -34,236 +34,87 @@
         {{-- start content --}}
        
         
-        {{-- fourth row --}}
-        <div class="col-lg-12">                
-            <div class="row p-4 rounded my-2" style="background-color: #c2cfe052;">                  
-                <div class="col-lg-12">
-                    <h4 class="font-weight-bold" style="color: var(--blue--text);">Media Statistics
-                    </h4>
-                </div>
-                <!-- Card Start -->
-                <div class="col-md-3 mb-3">
-                    <div class="statistics-card d-flex justify-content-between align-items-center shadow-sm">
-                        <div class="statistics-text">
-                            <div class="statistics-label">Media Views Today
-                            </div>
-                            <div class="statistics-value">125</div>
-                        </div>
-                        <div class="statistics-icon">
-                            <img src="{{ asset('assets/dashboard/img/media-view.png') }}" alt="icon">
-                        </div>
-                    </div>
-                </div>
-                <!-- Card End --> 
-                
-                <!-- Card Start -->
-                <div class="col-md-3 mb-3">
-                    <div class="statistics-card d-flex justify-content-between align-items-center shadow-sm">
-                        <div class="statistics-text">
-                            <div class="statistics-label">Media Views This Weeks
-                            </div>
-                            <div class="statistics-value">35</div>
-                        </div>
-                        <div class="statistics-icon">
-                            <img src="{{ asset('assets/dashboard/img/media-view.png') }}" alt="icon">
-                        </div>
-                    </div>
-                </div>
-                <!-- Card End -->
-                <!-- Card Start -->
-                <div class="col-md-3 mb-3">
-                    <div class="statistics-card d-flex justify-content-between align-items-center shadow-sm">
-                        <div class="statistics-text">
-                            <div class="statistics-label">Year to Date
-                            </div>
-                            <div class="statistics-value">125</div>
-                        </div>
-                        <div class="statistics-icon">
-                            <img src="{{ asset('assets/dashboard/img/calendar.png') }}" alt="icon">
-                        </div>
-                    </div>
-                </div>
-                <!-- Card End -->
+      {{-- fourth row --}}
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="table-responsive-xl mt-4">
+                    <table id="socialMediaTable" class="table table-bordered display" width="100%" >
+                        <thead class="bg-first">
+                        <tr>
+                                <th>Social Icon</th>
+                                <th>Social Platform</th>
+                                <th class="text-center">Clicks Today</th>
+                                <th class="text-center">Clicks This Week</th>
+                                <th class="text-center">Year to Date</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><div class="statistics-icon">
+                                    <img src="{{ asset('assets/dashboard/img/twitter-icon.png') }}" alt="icon">
+                                </div></td>
+                                <td>Twitter</td>
+                                <td class="text-center">25</td>
+                                <td class="text-center">125</td>
+                                <td class="text-center">1,500</td>
+                            </tr>
+                            <tr>
+                                <td><div class="statistics-icon">
+                                    <img src="{{ asset('assets/dashboard/img/instagram-icon.png') }}" alt="icon">
+                                </div></td>
+                                <td>Instagram</td>
+                                <td class="text-center">25</td>
+                                <td class="text-center">125</td>
+                                <td class="text-center">1,500</td>
+                            </tr>
+                            <tr>
+                                <td><div class="statistics-icon">
+                                    <img src="{{ asset('assets/dashboard/img/facebook-icon.png') }}" alt="icon">
+                                </div></td>
+                                <td>Facebook</td>
+                                <td class="text-center">25</td>
+                                <td class="text-center">125</td>
+                                <td class="text-center">1,500</td>
+                            </tr>
+                            <tr>
+                                <td><div class="statistics-icon">
+                                    <img src="{{ asset('assets/dashboard/img/click.png') }}" alt="icon">
+                                </div></td>
+                                <td class="text-right font-weight-bold" colspan="3">Total Clicks</td>
+                                <td class="text-center font-weight-bold">4,500</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div> 
             </div>
         </div>
-        {{-- end --}}
+      {{-- end --}}
+
         {{-- end content --}}
     {{-- middle content end here --}}
 </div>
     
 @endsection
 @push('script')
-<!-- file upload plugin start here -->
-
-
-
-    <!-- file upload plugin end here -->
 <script type="text/javascript" src="{{ asset('assets/plugins/parsley/parsley.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('assets/plugins/select2/select2.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('assets/plugins/toast-plugin/jquery.toast.min.js') }}"></script>
-
-
-<script type="text/javascript">
-
-    $('#userProfile').parsley({
-
-    });
-
-
-
-    $('#userProfile').on('submit', function(e) {
-        e.preventDefault();
-
-        var form = $(this);
-
-        if (form.parsley().isValid()) {
-
-            var url = form.attr('action');
-            var data = new FormData(form[0]);
-            $.ajax({
-                method: form.attr('method'),
-                url: url,
-                data: data,
-                contentType: false,
-                processData: false,
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                success: function(data) {
-                    if (!data.error) {
-                        $.toast({
-                            heading: 'Success',
-                            text: 'Details successfully saved',
-                            icon: 'success',
-                            loader: true,
-                            position: 'top-right', // Change it to false to disable loader
-                            loaderBg: '#9EC600' // To change the background
-                        });
-
-                    } else {
-                        $.toast({
-                            heading: 'Error',
-                            text: 'Records Not update',
-                            icon: 'error',
-                            loader: true,
-                            position: 'top-right', // Change it to false to disable loader
-                            loaderBg: '#9EC600' // To change the background
-                        });
-
-                    }
-                },
-
-            });
-        }
-    });
-    $('#city').select2({
-        allowClear: true,
-        placeholder :'Select City',
-        createTag: function(params) {
-            var term = $.trim(params.term);
-
-            if (term === '') {
-                return null;
-            }
-            return {
-                id: term,
-                text: term,
-                newTag: false // add additional parameters
-            }
-        },
-        tags: false,
-        minimumInputLength: 2,
-        tokenSeparators: [','],
-        ajax: {
-            url: "{{ route('city.list') }}",
-            dataType: "json",
-            type: "GET",
-            data: function(params) {
-                console.log(params);
-                var queryParameters = {
-                    query: params.term,
-                    state_id: $('#state').val()
-                }
-                return queryParameters;
-            },
-            processResults: function(data) {
-                return {
-                    results: $.map(data, function(item) {
-
-                        return {
-                            text: item.name,
-                            id: item.id
-                        }
-                    })
-                };
-            }
-        }
-    });
-
-    $('#state').select2({
-        allowClear: true,
-        placeholder :'Select State',
-        createTag: function(params) {
-            var term = $.trim(params.term);
-
-            if (term === '') {
-                return null;
-            }
-            return {
-                id: term,
-                text: term,
-                newTag: false // add additional parameters
-            }
-        },
-        tags: false,
-        minimumInputLength: 2,
-        tokenSeparators: [','],
-        ajax: {
-            url: "{{ route('state.list') }}",
-            dataType: "json",
-            type: "GET",
-            data: function(params) {
-                console.log(params);
-                var queryParameters = {
-                    query: params.term,
-                    country_id: $('#country').val()
-                }
-                return queryParameters;
-            },
-            processResults: function(data) {
-                return {
-                    results: $.map(data, function(item) {
-
-                        return {
-                            text: item.name,
-                            id: item.id
-                        }
-                    })
-                };
-            }
-        }
-    });
-
-
-    $('#country').on('change', function(e) {
-        if($(this).val()) {
-            $('#state').prop('disabled', false);
-            $('#state').select2('open');
-        } else {
-            $('#state').prop('disabled', true);
-        }
-    });
-
-    $('#state').on('change', function(e) {
-        if($(this).val()) {
-            $('#city').prop('disabled', false);
-            $('#city').select2('open');
-        } else {
-            $('#city').prop('disabled', true);
-        }
-    });
-
-
-</script>
-
+<script type="text/javascript" charset="utf8" src="{{ asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script>
+   $(document).ready(function() {
+       $('#socialMediaTable').DataTable({
+           responsive: true,
+           language: {
+               search: "Search: _INPUT_",
+               searchPlaceholder: "Search by ID or Profile Name...",
+               lengthMenu: "Show _MENU_ entries",
+               zeroRecords: "No matching records found",
+               info: "Showing _START_ to _END_ of _TOTAL_ entries",
+               infoEmpty: "No entries available",
+               infoFiltered: "(filtered from _MAX_ total entries)"
+           },
+           paging: true
+       });
+   });
+ </script>
 @endpush
