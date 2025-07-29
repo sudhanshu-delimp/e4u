f8783cc262ff2fba62cbe4eb145b7c2c8f29910c
jitender <jitender@delimp.com>
2025-07-29 12:55:51 +0530

    brb profile wip2

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -1187,9 +1187,13 @@ class WebController extends Controller
         $brb = new EscortBrb();
         if(request()->get('brb')){
             $brb = $brb->where('id', request()->get('brb'))->where('active', 'Y')->first();
+            dump(request()->get('brb'));
+            dump($brb);
         }
         
         $brb = $brb->where('profile_id', $id)->where('brb_time', '>', date('Y-m-d H:i:s'))->where('active', 'Y')->orderBy('brb_time', 'desc')->first();
+        dump($brb);
+        
         if($brb) {
             $brb = $brb->toArray(); 
         }
