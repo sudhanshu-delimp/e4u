2161fa542d1e78a21bf88d0cfbd3f53ef533a18d
shiv-delimp <shivendra@delimp.com>
2025-08-13 18:04:37 +0530

    task id  4329877 change icon in edit my account for 4 console)

--- resources/views/layouts/agent.blade.php
+++ resources/views/layouts/agent.blade.php
@@ -21,7 +21,7 @@
                 <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content custome_modal_max_width">
                         <div class="modal-header main_bg_color border-0">
-                            <h5 class="modal-title text-white"><img src="{{ asset('assets/dashboard/img/upload-photos.png') }}" class="custompopicon"> <span style="color:white" id="header_msg"></span></h5>
+                            <h5 class="modal-title text-white"><img src="{{ asset('assets/dashboard/img/save-setting.png') }}" class="custompopicon"> <span style="color:white" id="header_msg"></span></h5>
                             
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">
