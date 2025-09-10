2e9296427600485cbc9c6c795a591770fd6eba1d
raj_tiwari <raj.tiwari@delimp.com>
2025-09-10 15:49:38 +0530

    4111921 : Add CheckExpireDate helper function and refactor password expiry handling in EscortDashboardController.:

--- routes/agent.php
+++ routes/agent.php
@@ -10,10 +10,11 @@ use App\Http\Controllers\Agent\AgentTaskController;
 use App\Http\Controllers\Escort\ArchivesController;
 use App\Http\Controllers\Agent\AgentAccountController;
 use App\Http\Controllers\Agent\AgentRequestController;
+use App\Http\Controllers\Agent\AgentDashboardController;
 use App\Http\Controllers\Escort\EscortGalleryController;
 use App\Http\Controllers\Escort\Profile\CreateController;
-use App\Http\Controllers\Escort\Profile\UpdateController;
 
+use App\Http\Controllers\Escort\Profile\UpdateController;
 use App\Http\Controllers\Agent\AgentTourPaymentController;
 use App\Http\Controllers\Escort\EscortPolyPaymentController;
 use App\Http\Controllers\MyAdvertiser\ListAdvertiserController;
@@ -155,9 +156,7 @@ Route::get('advertisers',function(){
     return view('agent.dashboard.advertisers');
 })->name('agent.advertisers');
 
-Route::get('logs-and-status',function(){
-    return view('agent.dashboard.logs-and-status');
-})->name('agent.logs-and-status');
+
 
 Route::get('my-appointments',function(){
     return view('agent.dashboard.my-appointments');
@@ -226,3 +225,8 @@ Route::post('get-default-photos/{id}', [EscortGalleryController ::class, 'agentg
 Route::get('upload-avatar', [AgentController::class, 'uploadAvatar'])->name('upload-avatar');
 Route::post('agent-save-avatar/{id}', [AgentController::class, 'agentSaveAvatar'])->name('agent.save.avatar');
 Route::post('remove-avatar',[AgentController::class, 'agentRemoveAvatar'])->name('agent.avatar.remove');
+
+
+
+Route::get('logs-and-status', [AgentDashboardController::class, 'LogsAndStatus'])->name('agent.logs-and-status');
+Route::post('agent-update-password-duration', [AgentDashboardController::class, 'updatePasswordDuration'])->name('agent.update.password.duration');
