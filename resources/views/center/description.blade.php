2e122276bbf8775f12b9df5561ba3308235fecd5
subhamDelimp <subham@delimp.com>
2025-09-23 14:56:53 +0530

    Arriving

--- resources/views/center/description.blade.php
+++ resources/views/center/description.blade.php
@@ -147,8 +147,8 @@
                         </thead>
                         <tbody>
                             <tr>
-                                <td>25th May 2019</td>
-                                <td>1st June 2019</td>
+                                <td class="text-center">25th May 2019</td>
+                                <td class="text-center">1st June 2019</td>
                             </tr>
                         </tbody>
                     </table>
