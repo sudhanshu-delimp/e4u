5a956c2b4656a0b1e85f79d877014cd54a028227
jitender <jitender@delimp.com>
2025-10-01 16:15:04 +0530

    show reviews to all prpfile of the user

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -1168,6 +1168,11 @@ class WebController extends Controller
 
 
         $reviews = $escort->reviews;
+        $userEscortsProfile = Escort::where('user_id',$escort->user_id)->where('profile_name','!=',null)->where('enabled',1)->pluck('id');
+        if($userEscortsProfile ->isNotEmpty()){
+            $reviews = Reviews::whereIn('escort_id',$userEscortsProfile)->where('status','published')->get();
+        }
+
         // $reviews = Reviews::where('escort_id',$id)->where('status','approved')->with('user')->get()->unique('user_id');
         //dd($viewType);
         $user = DB::table('users')->where('id',(int)$escort->user_id)->select('contact_type')->first();
