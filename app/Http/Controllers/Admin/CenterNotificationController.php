251a821c368879e45370925ff256b9cd2c62e8d6
raj_tiwari <raj.tiwari@delimp.com>
2025-10-08 19:35:47 +0530

    Implement notification creation with date range validation and improve form handling

--- app/Http/Controllers/Admin/CenterNotificationController.php
+++ app/Http/Controllers/Admin/CenterNotificationController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\CenterNotification;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class CenterNotificationController extends Controller
@@ -12,6 +14,27 @@ class CenterNotificationController extends Controller
     }
 
     public function store(Request $request){
-        dd($request->all());
+        $data =  $request->only(['heading','start_date', 'finish_date', 'type', 'content', 'member_id']);
+        $start = Carbon::parse($data['start_date']);
+        $end =  Carbon::parse($data['finish_date']);
+        //Check condition 
+        $query = CenterNotification::where(function($q) use ($start, $end){
+                $q->whereBetween('start_date', [$start, $end])
+                ->orWhereBetween('finish_date', [$start, $end])
+                ->orWhere(function($q2) use ($start, $end){
+                    $q2->where('start_date', '<=', $start)
+                        ->where('finish_date', '>=', $end);
+                    });
+        });
+        if($query->exists()){
+             return error_response('A Notification already exists in the selected date range!', 422);
+        }
+        try {
+            CenterNotification::create($data);
+             return success_response($data, 'Notification create successfully!!');
+        } catch (\Exception $e) {
+            return error_response('Failed to create notification: ' . $e->getMessage(), 500);
+        }
+       
     }
 }
