1611a1b6151fe4e212413043f9d7c838e743f56d
shiv-delimp <shivendra@delimp.com>
2025-10-10 11:05:08 +0530

    task id 5440340 Change Data Format for all console

--- resources/views/user/dashboard/my-account.blade.php
+++ resources/views/user/dashboard/my-account.blade.php
@@ -71,7 +71,7 @@
                                                 <div class="form-group">
                                                     <label for="membership_num">Date Joined</label>
                                                     {{-- <input type="date" class="form-control" placeholder=" " aria-describedby="emailHelp"> --}}
-                                                    <label class="form-control form-back" placeholder=" " aria-describedby="emailHelp">{{Carbon\Carbon::parse($user->created_at)->format('d/m/Y')}}</label>
+                                                    <label class="form-control form-back" placeholder=" " aria-describedby="emailHelp">{{Carbon\Carbon::parse($user->created_at)->format('d-m-Y')}}</label>
                                                 </div>
                                             </div>
                                             <div class="col-md-6">
