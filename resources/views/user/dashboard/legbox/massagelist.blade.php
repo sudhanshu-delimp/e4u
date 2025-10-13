5e36b11b454d116d3190bdc0a5f3d9f45828f83c
shiv-delimp <shivendra@delimp.com>
2025-10-13 13:51:31 +0530

    task id 4466494 add hover over on alert and support ticket on navbar

--- resources/views/user/dashboard/legbox/massagelist.blade.php
+++ resources/views/user/dashboard/legbox/massagelist.blade.php
@@ -23,7 +23,7 @@
    <!-- Page Heading -->
    <div class="row">
       <div class="col-md-12 custom-heading-wrapper">
-            <h1 class="h1">Massage Center Legbox </h1>
+            <h1 class="h1">Massage Center Legbox  </h1>
             <span class="helpNoteLink" data-toggle="collapse" data-target="#notes" aria-expanded="true"><b>Help?</b></span>
       </div>
    </div>
