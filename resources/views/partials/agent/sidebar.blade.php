ff83606499d566dd95bca4071cb7ad2c383b0c2a
shiv-delimp <shivendra@delimp.com>
2025-10-22 17:17:45 +0530

    fixed conflict

--- resources/views/partials/agent/sidebar.blade.php
+++ resources/views/partials/agent/sidebar.blade.php
@@ -151,11 +151,11 @@
 
             <div class="py-0 collapse-inner rounded mb-2">
 
-                 <a class="collapse-item" href="{{ route('agent.my-income') }}">
+                 {{-- <a class="collapse-item" href="{{ route('agent.my-income') }}">
                     <img width="16" height="17" viewbox="0 0 16 17" fill="none"
                         src="{{ asset('assets/dashboard/img/menu-icon/file-document-multiple-outline.png') }}">
                     <span style="{{ request()->segment(3) == 'summary' ? 'color: #e5365a;' : '' }}">My Income</span>
-                </a>
+                </a> --}}
 
                 <a class="collapse-item" href="{{ route('Commision.statements') }}">
                     <img width="16" height="17" viewbox="0 0 16 17" fill="none"
