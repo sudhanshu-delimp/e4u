1e2e8b6071354822cdc947867cb7451bbcbcdeb1
shiv-delimp <shivendra@delimp.com>
2025-12-02 13:57:07 +0530

    task id 5579227 add verify and unverified icons in short list page

--- resources/views/user/dashboard/index.blade.php
+++ resources/views/user/dashboard/index.blade.php
@@ -10,7 +10,7 @@
                 @endforeach
             @endif
             <div class="custom-heading-wrapper col-md-12">
-                <h1 class="h1">Viewer Dashbaord</h1>
+                <h1 class="h1">Dashbaord</h1>
                 <span class="helpNoteLink" data-toggle="collapse" data-target="#notes" aria-expanded="true"><b>Help?</b></span>
             </div>
             <div class="col-md-12 mb-4">
