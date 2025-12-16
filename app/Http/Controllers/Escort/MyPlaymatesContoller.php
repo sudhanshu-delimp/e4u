dea041c051c67d01f380f6d96023acf6e5b0735d
Sudhanshu Bisht <sudhanshu@delimp.com>
2025-12-16 18:58:53 +0530

    sudhanshu, 5526796 implement child playmate table with remove option for each individual profile according to the work flow

--- app/Http/Controllers/Escort/MyPlaymatesContoller.php
+++ app/Http/Controllers/Escort/MyPlaymatesContoller.php
@@ -20,7 +20,7 @@ class MyPlaymatesContoller extends Controller
         return view('escort.dashboard.my-playmates');
     }
 
-    public function MyPlaymateDataTable(Request $request){
+    public function myPlaymateDataTable(Request $request){
         $conditions = [];
         list($result, $count, $other) = $this->playmate->paginatedGroupedList(
             request()->get('start'),
@@ -42,5 +42,51 @@ class MyPlaymatesContoller extends Controller
         );
 
         return response()->json($data);
+    }
+
+    public function getPlaymateListingsDataTable(Request $request){
+        $conditions = [];
+        $conditions['is_deleted'] = '0';
+        list($result, $count, $other) = $this->playmate->paginatedList(
+            $request->get('start'),
+            $request->get('length'),
+            $request->get('order')[0]['column'],
+            $request->get('order')[0]['dir'],
+            $request->get('columns'),
+            $request->get('search')['value'],
+            $request->get('escort_id'),
+            $conditions
+        );
+
+        $data = array(
+            "draw"            => intval(request()->input('draw')),
+            "recordsTotal"    => intval($count),
+            "recordsFiltered" => intval($count),
+            "other" => $other,
+            "data"            => $result
+        );
+
+        return response()->json($data);
+    }
+
+    public function trashPlaymateHistory(Request $request){
+        try {
+            $response['success'] = false;
+            $playmateHistoryId = $request->playmateHistoryId;
+            $history = $this->playmate->find($playmateHistoryId);
+            $history->escort->playmates()->detach($history->playmate_id); /** Remove Playmate from Escort's PlaymateList */
+            $history->playmate->playmates()->detach($history->escort_id); /** Remove Escort from Playmate's PlaymateList */
+            $this->playmate->trashPlaymateHistory($history->escort_id, $history->playmate_id); /** Update is_deleted in the playmate history table in reverse order  */
+            $response['success'] = true;
+            $response['message'] = "Removed successfully.";
+
+            return response()->json($response);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 }
--- routes/escort.php
+++ routes/escort.php
@@ -387,7 +387,9 @@ Route::get('code-of-conduct',function(){
 })->name('escort.code-of-conduct');
 
 Route::get('/my-playmates',[MyPlaymatesContoller::class,'index'])->name('escort.dashboard.my-playmates');
-Route::post('/my-playmates',[MyPlaymatesContoller::class, 'MyPlaymateDataTable'])->name('escort.dashboard.my-playmates');
+Route::post('/my-playmates',[MyPlaymatesContoller::class, 'myPlaymateDataTable'])->name('escort.dashboard.my-playmates');
+Route::post('/get-playmate-listings',[MyPlaymatesContoller::class, 'getPlaymateListingsDataTable'])->name('escort.dashboard.get-playmate-listings');
+Route::post('/trash-playmate-history',[MyPlaymatesContoller::class, 'trashPlaymateHistory'])->name('escort.dashboard.trash-playmate-history');
 
 Route::get('home-state/',[EscortController::class, 'homeState'])->name('escort.home-state');
 Route::post('add-playmate/{id}',[ProfileInformationController::class, 'savePlaymate'])->name('escort.add.playmate');
