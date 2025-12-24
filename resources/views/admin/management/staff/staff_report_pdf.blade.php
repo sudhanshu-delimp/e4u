1e0aa7660fecb1324be12b45633065268090bd44
Mithilesh Kumar <mithilesh@delimp.com>
2025-12-24 12:17:58 +0530

    Fixed the css issue into staff page

--- resources/views/admin/management/staff/staff_report_pdf.blade.php
+++ resources/views/admin/management/staff/staff_report_pdf.blade.php
@@ -13,13 +13,13 @@
         href="https://fonts.googleapis.com/css2?family=wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
  
-    <link href="{{ asset('assets/dashboard/vendor/file-upload/css/fill-profile-details.css') }}" rel="stylesheet"
+    <link href="{{ url('/').'/'.'assets/dashboard/vendor/file-upload/css/fill-profile-details.css' }}" rel="stylesheet"
         type="text/css">
 
     <!-- Custom styles for this template-->
-    <link href="{{ asset('assets/dashboard/css/sb-admin-2.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/dashboard/css/dk-style.css?v1.4') }}" rel="stylesheet">
-    <link href="{{ asset('assets/dashboard/css/dk-responsive.css') }}" rel="stylesheet">
+    <link href="{{ url('/').'/'.'assets/dashboard/css/sb-admin-2.min.css' }}" rel="stylesheet">
+    <link href="{{ url('/').'/'.'assets/dashboard/css/dk-style.css?v1.4' }}" rel="stylesheet">
+    <link href="{{ url('/').'/'.'assets/dashboard/css/dk-responsive.css' }}" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <style>
         h2 {
@@ -73,6 +73,8 @@
             padding: 0px !important;
             border-radius: 10px;
         }
+        h2{font-size:22px }
+        h6{font-size:18px}
     </style>
 </head>
 
