786f3564d1b0b952033c4a3ca72b44ddebcb3c35
Mithilesh Kumar <mithilesh@delimp.com>
2025-12-24 12:35:03 +0530

    Fixed the css issue

--- resources/views/admin/management/staff/staff_report_pdf.blade.php
+++ resources/views/admin/management/staff/staff_report_pdf.blade.php
@@ -1,30 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Staff Member Report</title>
 
- 
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <link
         href="https://fonts.googleapis.com/css2?family=wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
- 
-    <link href="{{ url('/').'/'.'assets/dashboard/vendor/file-upload/css/fill-profile-details.css' }}" rel="stylesheet"
+
+    <link href="{{ url('/') . '/' . 'assets/dashboard/vendor/file-upload/css/fill-profile-details.css' }}" rel="stylesheet"
         type="text/css">
 
     <!-- Custom styles for this template-->
-    <link href="{{ url('/').'/'.'assets/dashboard/css/sb-admin-2.min.css' }}" rel="stylesheet">
-    <link href="{{ url('/').'/'.'assets/dashboard/css/dk-style.css?v1.4' }}" rel="stylesheet">
-    <link href="{{ url('/').'/'.'assets/dashboard/css/dk-responsive.css' }}" rel="stylesheet">
+    <link href="{{ url('/') . '/' . 'assets/dashboard/css/sb-admin-2.min.css' }}" rel="stylesheet">
+    <link href="{{ url('/') . '/' . 'assets/dashboard/css/dk-style.css?v1.4' }}" rel="stylesheet">
+    <link href="{{ url('/') . '/' . 'assets/dashboard/css/dk-responsive.css' }}" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <style>
-        h2 {
-            font-weight: bold
-        }
         .info-item {
             background: #f8faff;
             border-radius: 8px;
@@ -56,15 +54,17 @@
             font-weight: 500;
             line-height: 1px;
         }
+
         @page {
             size: A4;
         }
 
         /* Header */
-        
+
         .page-number:before {
             content: counter(page);
         }
+
         .my-account-card {
             background-color: #fff;
             box-shadow: 0 4px 16px rgba(0, 0, 0, 0.08);
@@ -73,8 +73,25 @@
             padding: 0px !important;
             border-radius: 10px;
         }
-        h2{font-size:22px }
-        h6{font-size:18px}
+
+        h2 {
+            font-size: 22px;
+            font-weight: bold;
+            padding-bottom: 10px;
+        }
+
+        h6 {
+            font-size: 18px;
+        }
+
+        table td {
+            padding: 7px .75rem !important;
+        }
+
+        table th {
+            padding: 7px .75rem !important;
+            font-weight: 500
+        }
     </style>
 </head>
 
@@ -94,23 +111,25 @@
             $cities = config('escorts.profile.cities');
             $city = isset($cities[$staff->city_id]) ? $cities[$staff->city_id] : '';
 
-                 $setting = $staff->staff_setting??null;
-        $idle_preference_times = config('staff.idle_preference_time');
-        $idle_preference_time = "";
-         $twofa = "";
-        if(isset( $setting) && (isset($setting->idle_preference_time))) {
-            $idle_preference_time = isset($idle_preference_times[(string)$setting->idle_preference_time]) ? $idle_preference_times[$setting->idle_preference_time] : "";
-        }
-        $twofas = config('staff.twofa');
-        if(isset( $setting) && isset($setting->twofa)) {
-        $twofa = isset($twofas[$setting->twofa]) ? $twofas[$setting->twofa] : "";
-        }
+            $setting = $staff->staff_setting ?? null;
+            $idle_preference_times = config('staff.idle_preference_time');
+            $idle_preference_time = '';
+            $twofa = '';
+            if (isset($setting) && isset($setting->idle_preference_time)) {
+                $idle_preference_time = isset($idle_preference_times[(string) $setting->idle_preference_time])
+                    ? $idle_preference_times[$setting->idle_preference_time]
+                    : '';
+            }
+            $twofas = config('staff.twofa');
+            if (isset($setting) && isset($setting->twofa)) {
+                $twofa = isset($twofas[$setting->twofa]) ? $twofas[$setting->twofa] : '';
+            }
         @endphp
         <div class="col-md-12 ">
             <div class="my-account-card">
                 <div class="card-head" style="display: flex; justify-content:space-between;align-items:center;">
                     <h2>Staff Member Repord</h2>
-                   
+
                 </div>
 
                 <h6 class=" pb-1 text-blue-primary">Personal Details</h6>
@@ -144,92 +163,92 @@
                 </table>
 
                 <!-- Next of Kin Section -->
-              
-                    <h6 class=" pb-1 text-blue-primary">Next of Kin (Emergency Contact)</h6>
 
-                    <table class="table table-bordered mb-3">
-                        <tbody>
-                            <tr>
-                                <th width="40%">Name of Kin</th>
-                                <td width="60%">{{ $staff->staff_detail->kin_name }}</td>
-                            </tr>
-                            <tr>
-                                <th>Relationship</th>
-                                <td> {{ $staff->staff_detail->kin_relationship }}</td>
-                            </tr>
-                            <tr>
-                                <th>Mobile</th>
-                                <td>{{ $staff->staff_detail->kin_mobile }}</td>
-                            </tr>
-                            <tr>
-                                <th>Email (optional)</th>
-                                <td>{{ $staff->staff_detail->kin_email }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-            
-                <!-- Section: Other Details -->
-               
-                    <h6 class="pb-1 text-blue-primary">Other Details</h6>
+                <h6 class=" pb-1 text-blue-primary">Next of Kin (Emergency Contact)</h6>
 
-                    <table class="table table-bordered mb-3">
-                        <tbody>
-                            <tr>
-                                <th>Security Level</th>
-                                <td>{{ $securityLevel }}</td>
-                            </tr>
-                            <tr>
-                                <th width="40%">Position</th>
-                                <td width="60%">{{ $staff->staff_detail->position($staff->staff_detail->position) }}
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>Location</th>
-                                <td> {{ $city }}</td>
-                            </tr>
-                            <tr>
-                                <th>Commenced Date</th>
-                                <td>{{ showDateWithFormat($staff->staff_detail->commenced_date, 'd-m-Y') }}</td>
-                            </tr>
-
-                            <tr>
-                                <th>Employment Status</th>
-                                <td>{{ $employmentStatus }}</td>
-                            </tr>
-                            <tr>
-                                <th>Employment Agreement</th>
-                                <td>{{ ucfirst($staff->staff_detail->employment_agreement) }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-            
-                <!-- Section: Building Security -->
-             
-                    <h6 class="pb-1 text-blue-primary">Building Security</h6>
-                    <table class="table table-bordered mb-3">
-                        <tbody>
-                            <tr>
-                                <th width="40%">Building Security</th>
-                                <td width="60%">{{ ucfirst($staff->staff_detail->building_access_code) }}</td>
-                            </tr>
-                            <tr>
-                                <th>Key Provided?</th>
-                                <td> {{ ucfirst($staff->staff_detail->keys_issued) }}</td>
-                            </tr>
-                            <tr>
-                                <th>Car Park?</th>
-                                <td>{{ ucfirst($staff->staff_detail->car_parking) }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-
-                   
-               
                 <table class="table table-bordered mb-3">
                     <tbody>
                         <tr>
+                            <th width="40%">Name of Kin</th>
+                            <td width="60%">{{ $staff->staff_detail->kin_name }}</td>
+                        </tr>
+                        <tr>
+                            <th>Relationship</th>
+                            <td> {{ $staff->staff_detail->kin_relationship }}</td>
+                        </tr>
+                        <tr>
+                            <th>Mobile</th>
+                            <td>{{ $staff->staff_detail->kin_mobile }}</td>
+                        </tr>
+                        <tr>
+                            <th>Email (optional)</th>
+                            <td>{{ $staff->staff_detail->kin_email }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <!-- Section: Other Details -->
+
+                <h6 class="pb-1 text-blue-primary">Other Details</h6>
+
+                <table class="table table-bordered mb-3">
+                    <tbody>
+                        <tr>
+                            <th>Security Level</th>
+                            <td>{{ $securityLevel }}</td>
+                        </tr>
+                        <tr>
+                            <th width="40%">Position</th>
+                            <td width="60%">{{ $staff->staff_detail->position($staff->staff_detail->position) }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Location</th>
+                            <td> {{ $city }}</td>
+                        </tr>
+                        <tr>
+                            <th>Commenced Date</th>
+                            <td>{{ showDateWithFormat($staff->staff_detail->commenced_date, 'd-m-Y') }}</td>
+                        </tr>
+
+                        <tr>
+                            <th>Employment Status</th>
+                            <td>{{ $employmentStatus }}</td>
+                        </tr>
+                        <tr>
+                            <th>Employment Agreement</th>
+                            <td>{{ ucfirst($staff->staff_detail->employment_agreement) }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <!-- Section: Building Security -->
+
+                <h6 class="pb-1 text-blue-primary">Building Security</h6>
+                <table class="table table-bordered mb-3">
+                    <tbody>
+                        <tr>
+                            <th width="40%">Building Security</th>
+                            <td width="60%">{{ ucfirst($staff->staff_detail->building_access_code) }}</td>
+                        </tr>
+                        <tr>
+                            <th>Key Provided?</th>
+                            <td> {{ ucfirst($staff->staff_detail->keys_issued) }}</td>
+                        </tr>
+                        <tr>
+                            <th>Car Park?</th>
+                            <td>{{ ucfirst($staff->staff_detail->car_parking) }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
+
+                <table class="table table-bordered " style="padding-top: 10px;">
+                    <tbody>
+                        <tr>
                             <th width="40%">Idle Time Preference</th>
-                            <td width="60%">{{$idle_preference_time}}</td>
+                            <td width="60%">{{ $idle_preference_time }}</td>
                         </tr>
                         <tr>
                             <th width="40%">2FA Authentication</th>
@@ -237,9 +256,10 @@
                         </tr>
                     </tbody>
                 </table>
-           
-                </div>
+
+            </div>
         </div>
     </div>
 </body>
+
 </html>
