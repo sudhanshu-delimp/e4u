eed1b0edebd4db934386d5c62ae381b65fdcb34e
raj_tiwari <raj.tiwari@delimp.com>
2025-12-29 13:58:53 +0530

    Add global notifications functionality to navigation components

---
+++ resources/views/components/global_frontend/global-alert.blade.php
@@ -0,0 +1,13 @@
+
+<div class="alert alert-custom alert-dismissible fade show border-bottom text-center" role="alert">
+    
+    <!-- Heading row -->
+    <div class="font-weight-bold mb-1">
+        {{ $heading }}
+    </div>
+
+    <!-- Content row -->
+    <div>
+        {{ $content }}
+    </div>
+</div>
--- resources/views/partials/web/navigation3.blade.php
+++ resources/views/partials/web/navigation3.blade.php
@@ -1,3 +1,10 @@
+
+@if(global_notifications())
+    @foreach(global_notifications() as $notification)
+        <x-global_frontend.global-alert :heading="$notification['heading']" :content="$notification['content'] ?? $notification['template_name']" />
+    @endforeach
+@endif
+
 <div class="blue-bg-before home--header">
          <nav class="navbar navbar-expand-lg navbar-light blue_and_white_nav_blue_in_small">
          <div class="container-fluid manage_header_padding">
@@ -167,12 +174,7 @@
 </div>
 
 
-<div class="alert alert-custom alert-dismissible fade show" role="alert">
-  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>
+
 
 @push('scripts')
     <script>
