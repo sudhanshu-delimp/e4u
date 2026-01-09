75d9aad22d09626456181adeaf9ec18f681f4fbf
shiv-delimp <shivendra@delimp.com>
2026-01-09 15:12:34 +0530

    design shareholder page 5578862

--- resources/views/shareholder/dashboard/blackbox-tech/directors.blade.php
+++ resources/views/shareholder/dashboard/blackbox-tech/directors.blade.php
@@ -6,7 +6,7 @@
 
 <div class="container-fluid pl-3 pl-lg-5 pr-3 pr-lg-5">
     <!-- Page Heading -->
-    
+
     <div class="d-sm-flex align-items-center justify-content-between">
         <div class="custom-heading-wrapper">
             <h1 class="h1">Directors</h1>
@@ -14,10 +14,10 @@
         </div>
         <div class="back-to-dashboard">
             @if (request('from') !== 'sidebar')
-            <a href="{{ url()->previous() ?? route('dashboard.home') }}">
-                <img src="{{ asset('assets/dashboard/img/crossimg.png') }}" alt="Back To Dashboard">
-            </a>
-            @endif 
+                <a href="{{ url()->previous() ?? route('dashboard.home') }}">
+                    <img src="{{ asset('assets/dashboard/img/crossimg.png') }}" alt="Back To Dashboard">
+                </a>
+            @endif
         </div>
     </div>
     <div class="row">
@@ -25,19 +25,93 @@
             <div class="card collapse" id="notes" style="">
                 <div class="card-body">
                     <p class="mb-0" style="font-size: 20px;"><b>Notes:</b> </p>
-                    <p></p>
                     <ol>
-                        
+                        <li>The Company’s Directors details are set out here.</li>
+                        <li>The Directors are available to Shareholders to discuss any aspects of the Company,
+                            subsidiaries and related entities.</li>
                     </ol>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
+    {{-- content start --}}
+
+    <div class="row sh_directors">
+        <div class="col-md-12 mt-2">
+            <div id="table-sec" class="table-responsive-xl">
+                <table class="table" id="AgentReportTable">
+                    <thead class="table-bg">
+                        <tr>
+                            <th>Full Name & Address </th>
+                            <th>Positions Held</th>
+                            <th>Meeting Status</th>
+                            <th>Other Directorships<sup>(1)</sup></th>
+                            <th>Mobile</th>
+                            <th>Email<sup>(2)</sup></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <p>Wayne David Primrose</p>
+                                <p>188 Almeria Parade Upper Swan WA 6069</p>
+                            </td>
+                            <td>
+                                <p>Director</p>
+                            </td>
+                            <td>
+                                <p>Chairman</p>
+                                <p>Attendee</p>
+                            </td>
+                            <td>
+                                <p>Agency Management (Australia) Pty Ltd</p>
+                                <p>PEAMS Australia Pty Ltd</p>
+                            </td>
+                            <td>0438 028 728</td>
+                            <td>
+                            
+                             <a href="{{ asset('assets/dashboard/forms-pdf/E4U_Coin_2026.png') }}"
+                                                target="_blank" class="custom_links_design"
+                                                download="E4U_Coin_2026.png">wayne@blackboxtech.com.au</a>
+                            </td>
+
+                        </tr>
+
+                          <tr>
+                            <td>
+                                <p>Andrew Stewart Stephen</p>
+                                <p>32 Astral Avenue Carlisle WA 6101</p>
+                            </td>
+                            <td>
+                                <p>Director</p>
+                            </td>
+                            <td>
+                                <p>Attendee</p>
+                            </td>
+                            <td>
+                                
+                                <p>PEAMS Australia Pty Ltd</p>
+                            </td>
+                            <td>0401 071 478</td>
+                            <td>
+                            
+                             <a href="{{ asset('assets/dashboard/forms-pdf/E4U_Coin_2026.png') }}"
+                                                target="_blank" class="custom_links_design"
+                                                download="E4U_Coin_2026.png">andrew@blackboxtech.com.au</a>
+                            </td>
+
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    {{-- end --}}
+
+</div>
 @endsection
 @section('script')
-<script>
-    
-</script>
+<script></script>
 @endsection
