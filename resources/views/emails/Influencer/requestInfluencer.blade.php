3fe7651187e1cd63655d2558bdfdfd1d075506d9
Sudhanshu Bisht <sudhanshu@delimp.com>
2026-01-12 13:10:37 +0530

    sudhanshu, replace logo path , while testing the mail function on the staging server

--- resources/views/emails/Influencer/requestInfluencer.blade.php
+++ resources/views/emails/Influencer/requestInfluencer.blade.php
@@ -18,7 +18,7 @@
                 <tr>
                   <td style="text-align: left;">
                     
-                    <img src="{{ asset('assets/app/img/logo.png') }}" alt="E4U Logo" style="height: 50px;">
+                    <img src="{{ asset('images/logo.png') }}" alt="E4U Logo" style="height: 50px;">
                   </td>
                   <td style="text-align: right; color: #ffffff; font-size: 16px; font-weight: bold;">
                     Request to be an Influencer  {{$body['member_id']}}<br>
