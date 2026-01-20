215196cf121fa3939767eaf3a5afe878baf20409
shiv-delimp <shivendra@delimp.com>
2026-01-20 18:37:34 +0530

    dashboard new change

--- resources/views/admin/management/dashboard.blade.php
+++ resources/views/admin/management/dashboard.blade.php
@@ -2,34 +2,31 @@
 @section('style')
 @stop
 @section('content')
-<div id="wrapper">
-   <div id="content-wrapper" class="d-flex flex-column">
-      <div id="content">
-         
-         <div class="container-fluid pl-3 pl-lg-5 pr-3 pr-lg-5">
-            <div class="row">
-               <div class="custom-heading-wrapper col-md-12">
-                  <h1 class="h1">Dashboard</h1>
-                     <span class="helpNoteLink" data-toggle="collapse" data-target="#notes"><b>Help?</b> </span>
-               </div>
-               <div class="col-md-12 mb-5 collapse" id="notes">
-                  <div class="card">
-                     <div class="card-body">
-                        <h3 class="NotesHeader"><b>Notes:</b> </h3>
-                        <ol class="level-1">
-                          
-                            </ol>
-                          </li>
-                        </ol>
-                     </div>
-                  </div>
-               </div>
-            </div>
-      </div>
-   </div>
-</div>
-@endsection
+    <div id="wrapper">
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
 
-@push('script')
-  
-@endpush
+                <div class="container-fluid pl-3 pl-lg-5 pr-3 pr-lg-5">
+                    <div class="row">
+                        <div class="custom-heading-wrapper col-md-12">
+                            <h1 class="h1">Dashboard</h1>
+                            <span class="helpNoteLink" data-toggle="collapse" data-target="#notes"><b>Help?</b> </span>
+                        </div>
+                        <div class="col-md-12 mb-5 collapse" id="notes">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h3 class="NotesHeader"><b>Notes:</b></h3>
+                                       <ol>
+
+                                       </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
+
+    @push('script')
+    @endpush
