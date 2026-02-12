c68c031a7aa1b99978b4aa4508e96d0542a5be36
Rizwan <rizwan.khan@delimp.com>
2026-02-12 12:24:26 +0530

    auto-complete

--- resources/views/escort/dashboard/change-password.blade.php
+++ resources/views/escort/dashboard/change-password.blade.php
@@ -50,7 +50,7 @@
                 </div>
                 <div class="col-md-12">
                     <form class="v-form-design" id="userProfile" action="{{ route('escort.update.password') }}"
-                        method="POST" autocomplete="off">>
+                        method="POST" autocomplete="off">
                         @csrf
                         <div class="row">
                             <div class="col-md-6">
