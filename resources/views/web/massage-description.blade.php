fc55f24be3c84c73f69bd8060f9214737b7d5007
shiv-delimp <shivendra@delimp.com>
2026-02-18 11:12:24 +0530

    add map in massge description page

--- resources/views/web/massage-description.blade.php
+++ resources/views/web/massage-description.blade.php
@@ -261,7 +261,7 @@
                         <div style="width: 100%">
                             <iframe width="100%" height="153" frameborder="0" scrolling="no" marginheight="0"
                                 marginwidth="0"
-                                src="https://maps.google.com/maps?width=100%25&height=600&hl=en&q=nema%20san%20francisco&t=&z=14&ie=UTF8&iwloc=B&output=embed"
+                                src="https://maps.google.com/maps?width=100%25&height=600&hl=en&q=Perth%20Western%20Australia&t=&z=14&ie=UTF8&iwloc=B&output=embed"
                                 style="filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));">
                             </iframe>
                         </div>
