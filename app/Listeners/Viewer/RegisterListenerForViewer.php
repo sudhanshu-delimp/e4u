be8c827fd37e3eac68292307d5394e42878ce9ea
raj_tiwari <raj.tiwari@delimp.com>
2026-03-09 17:49:14 +0530

    create new email template when register viewer first time

--- app/Listeners/Viewer/RegisterListenerForViewer.php
+++ app/Listeners/Viewer/RegisterListenerForViewer.php
@@ -3,12 +3,13 @@
 namespace App\Listeners\Viewer;
 
 use App\Events\ViewerRegister;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Mail\NewUserRegistrationConfirmation;
+use App\Mail\NewUserRegistrationConfirmationToAdmin;
 use App\Mail\RegisterEmailForViewer;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
 
 class RegisterListenerForViewer
 {
@@ -33,5 +34,6 @@ class RegisterListenerForViewer
         $user = $event->user;
         Mail::to($user->email)->send(new RegisterEmailForViewer($user));
         Mail::to($user->email)->later(now()->addSeconds(5), new NewUserRegistrationConfirmation($user));
+        Mail::to(config('common.contactus_admin_email'))->later(now()->addSeconds(5), new NewUserRegistrationConfirmationToAdmin($user));
     }
 }
