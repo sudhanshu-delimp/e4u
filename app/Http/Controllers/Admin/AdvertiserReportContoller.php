13688e20de684a5b3ee98db914328612beb15ba0
Rizwan <rizwan.khan@delimp.com>
2026-03-10 12:38:12 +0530

    mc-report

--- app/Http/Controllers/Admin/AdvertiserReportContoller.php
+++ app/Http/Controllers/Admin/AdvertiserReportContoller.php
@@ -52,21 +52,61 @@ class AdvertiserReportContoller extends Controller
     {
         [$advertiserReports, $reports] = $this->getAdvertiserReports();
 
-        $advertiserReports = ReportEscortProfile::with('escort.user')
-            ->orderByRaw("CASE WHEN report_status = 'pending' THEN 1 WHEN report_status = 'resolved' THEN 2 END")
-            ->orderBy('updated_at', 'desc');
+        // $advertiserReports = ReportEscortProfile::with('escort.user')
+        //     ->orderByRaw("CASE WHEN report_status = 'pending' THEN 1 WHEN report_status = 'resolved' THEN 2 END")
+        //     ->orderBy('updated_at', 'desc');
+
+
+             $advertiserReports = ReportEscortProfile::with(['escort','massage','viewer'])
+        ->orderByRaw("CASE WHEN report_status = 'pending' THEN 1 WHEN report_status = 'resolved' THEN 2 END")
+        ->orderBy('updated_at', 'desc');
+       
 
         return DataTables::of($advertiserReports)
-            ->addColumn('ref', fn($row) => $row->id . ($row->escort->id ?? ''))
-            
-            ->addColumn('member_id', fn($row) => $row->escort->user->member_id ?? '-')
+
+
+            ->addColumn('ref', function ($row) {
+
+                if ($row->advertiser_type == 'escort') {
+                    return $row->id . (optional($row->escort)->id ?? '');
+                }
+
+                if ($row->advertiser_type == 'massage') {
+                    return $row->id . (optional($row->massage)->id ?? '');
+                }
+
+                return $row->id;
+            })
+
+            ->addColumn('member_id', function ($row) {
+
+                if ($row->advertiser_type == 'escort') {
+                    return optional(optional($row->escort)->user)->member_id ?? '-';
+                }
+
+                if ($row->advertiser_type == 'massage') {
+                    return optional(optional($row->massage)->user)->member_id ?? '-';
+                }
+
+                return '-';
+            })
+
             ->addColumn('report_type', fn($row) => formatStringTitleCase($row->report_tag) ?? '-')
-            // ->addColumn('mobile', fn($row) => $row->escort->user->phone ?? '-')
-            // ->addColumn('home_state', fn($row) => $row->escort->user->home_state ?? '-')
-            ->addColumn('advertiser_id', fn($row) => $row->escort->id ?? '-')
-            ->addColumn('stage_name', fn($row) => $row->escort->name ?? '-')
+            ->addColumn('advertiser_id', function ($row) {
+                return $row->viewer->member_id;
+            })
+            ->addColumn('stage_name', function ($row) {
+
+                if ($row->advertiser_type == 'escort') {
+                    return  (optional($row->escort)->name ?? '');
+                }
+
+                if ($row->advertiser_type == 'massage') {
+                    return  (optional($row->massage)->profile_name ?? '');
+                }               
+            })
+
             ->addColumn('date', fn($row) => date('d-m-Y', strtotime($row->created_at)))
-            // ->addColumn('status', fn($row) => $row->report_status == 'pending' ? 'Active' : 'Resolved')
             ->addColumn('status', function ($row) {
                 $statusText = $row->report_status == 'pending' 
                     ? 'Active' 
--- app/Http/Controllers/ReportMassageController.php
+++ app/Http/Controllers/ReportMassageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\AttemptLogin;
 use App\Models\MassageLike;
 use App\Models\MassageProfile;
+use App\Models\ReportEscortProfile;
 use App\Models\ReportMassageProfile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -36,8 +37,12 @@ class ReportMassageController extends Controller
             $massage_id = $request->massage_id;
             $viewer_id = Auth::user()->id;
 
-            $res = ReportMassageProfile::where('massage_id', $massage_id)->where( 
-                    'viewer_id', $viewer_id)->select('report_desc','report_tag')->first();
+
+             $res = ReportEscortProfile::where('advertiser_id', $massage_id)
+                            ->where('advertiser_type', 'massage')
+                            ->where('viewer_id', $viewer_id)
+                            ->select('report_desc','report_tag')->first();
+
 
             $data = array(
                 'status'=>200,
@@ -69,17 +74,20 @@ class ReportMassageController extends Controller
             $report_tag = $request->report_tag;
             $report_desc = $request->description;
 
-            $res = ReportMassageProfile::updateOrCreate(
+           
+
+            $res = ReportEscortProfile::updateOrCreate(
                 [
-                    'massage_id' => $massage_id,  
-                    'viewer_id' => $viewer_id, 
+                    'advertiser_id'   => $massage_id,
+                    'viewer_id'       => $viewer_id,
+                    'advertiser_type' => 'massage',
                 ],
                 [
-                    'report_tag'     => $report_tag,  
-                    'report_desc'    => $report_desc, 
-                    'admin_id'       => null,
-                    'report_status'  => 'pending',
-                    'action_message' => null,
+                    'report_tag'      => $report_tag,
+                    'report_desc'     => $report_desc,
+                    'admin_id'        => null,
+                    'report_status'   => 'pending',
+                    'action_message'  => null,
                 ]
             );
 
