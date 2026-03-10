43a3d152f7207615a45c8073935c9b4316e06373
shiv-delimp <shivendra@delimp.com>
2026-03-10 18:48:26 +0530

    fixed update verification image to use pending icon

--- resources/views/web/mc/massage-description.blade.php
+++ resources/views/web/mc/massage-description.blade.php
@@ -606,7 +606,7 @@
                                                             @endforeach
 
                                                     <div class="veryfy_img">
-                                                        <img src="{{ asset('../assets/app/img/verify/unverified_light.png') }}">
+                                                        <img src="{{ asset('../assets/app/img/pending_icon/e4u_pending_REV.svg') }}">
                                                     </div>
                                                 </div>
 
