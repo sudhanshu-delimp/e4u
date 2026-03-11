ec5c02a71294680b252b9636ca2019fa5e851e67
Rizwan <rizwan.khan@delimp.com>
2026-03-11 13:08:50 +0530

    add-listing

--- resources/views/center/dashboard/listing/add-listing.blade.php
+++ resources/views/center/dashboard/listing/add-listing.blade.php
@@ -10,7 +10,111 @@
    .swal-button {
    background-color: #242a2c;
    }
+
+.eachListing:first-child .removeCross{
+    display:none;
+}
+
+.customModal{
+display:none;
+position:fixed;
+top:0;
+left:0;
+width:100%;
+height:100%;
+background:rgba(0,0,0,0.4);
+z-index:9999;
+}
+
+.summary-container{
+background:#f6f7f9;
+width:70%;
+padding:20px;
+border-radius:6px;
+
+/* perfect center */
+position:absolute;
+top:50%;
+left:50%;
+transform:translate(-50%,-50%);
+}
+
+.summary-header{
+background: var(--blue--text);
+color:white;
+padding:15px 20px;
+font-size:20px;
+display:flex;
+justify-content:space-between;
+align-items:center;
+}
+
+.member-id{
+font-size:14px;
+}
+
+.summary-table{
+width:100%;
+border-collapse:collapse;
+margin-top:20px;
+}
+
+.summary-table th{
+background: var(--blue--text);
+color:white;
+padding:12px;
+border:1px solid #cfd6e0;
+font-weight:500;
+text-align: center;
+}
+
+.summary-table td{
+padding:12px;
+border:1px solid #cfd6e0;
+text-align:center;
+background:#fff;
+}
+
+.total-row td{
+background:#f4f6f9;
+font-weight:600;
+}
+
+.pay-area{
+text-align:right;
+margin-top:25px;
+}
+
+.close-btn{
+background:#FF3C5F;
+border:none;
+padding:10px 20px;
+border-radius:4px;
+margin-right:10px;
+cursor:pointer;
+}
+
+.close-btn:hover{
+background:#FF3C5F;
+}
+
+.pay-btn{
+background:#0f2745;
+color:#fff;
+border:none;
+padding:10px 25px;
+border-radius:5px;
+cursor:pointer;
+}
+
+.pay-btn:hover{
+background:#16385f;
+}
    
+.member-id .pr-2 i {
+    color:#fff !important;
+}
+
 </style>
 @stop
 @section('content')
@@ -105,7 +209,7 @@
             
                     <!-- Footer -->
                     <div class="listing-footer" style="text-align:right; margin-top:20px;">
-                        <button type="submit" class="save_profile_btn" id="escort-form-submit-btn" disabled="true">Proceed to Payment</button>
+                        <button type="button" class="save_profile_btn" id="escort-form-submit-btn" disabled="true">Proceed to Payment</button>
                     </div>
                 </form>
 
@@ -114,6 +218,41 @@
         </div>
     </div>
     
+</div>
+
+
+<!-- Payment Summary Modal -->
+<div id="summaryModal" class="customModal">
+
+        <div class="summary-container">
+        <div class="summary-header">
+        <span>Summary</span>
+        <span class="member-id"> <span class="pr-2 "><i class="fa fa-user"></i></span> Member ID: E20118</span>
+        </div>
+
+        <table class="summary-table" >
+                    <thead>
+                        <tr>
+                        <th>Listing</th>
+                        <th>Stage Name</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Days</th>
+                        <th>Rate</th>
+                        <th>Full Fee</th>
+                        <th>Discount</th>
+                        <th>Discounted Fee</th>
+                        </tr>
+                    </thead>
+                    <tbody id="summaryBody"></tbody>
+        </table>
+
+        <div class="pay-area">
+        <button class="close-btn">Close</button>
+        <button class="pay-btn">Pay</button>
+        </div>
+     </div>
+
 </div>
 
 @endsection
@@ -126,8 +265,27 @@
 
 
 <script type="text/javascript">
+let profileCount = {{ count($profiles) }};
 
 $(document).ready(function () {
+    
+    if(profileCount === 0){
+
+        Swal.fire({
+            icon: 'warning',
+            title: 'Listings',
+            text: 'You have no profile. Please create a profile first.',
+            confirmButtonText: 'OK'
+        }).then((result) => {
+
+            if(result.isConfirmed){
+                window.location.href = "{{ url('center-dashboard/create-profile') }}";
+            }
+
+        });
+
+    }
+
 
     function checkAllRows() 
     {
@@ -153,6 +311,7 @@ $(document).ready(function () {
     $(document).on('change', '.profile_start, .profile_end, select[name="massage_id[]"]', function () 
     {
 
+        updateProfileOptions();  
         var is_form_valid = true;
         let row = $(this).closest('.eachListing');
         let start = row.find('.profile_start').val();
@@ -210,6 +369,7 @@ $(document).ready(function () {
  
     $("#add_listing").click(function () {
 
+       
         let lastRow = $(".eachListing").last();
         let lastEndDate = lastRow.find(".profile_end").val();
         let newRow = lastRow.clone();
@@ -219,6 +379,7 @@ $(document).ready(function () {
         newRow.find(".js_datepicker").removeClass("hasDatepicker").removeAttr("id");
 
         $(".eachListing").last().after(newRow);
+         updateProfileOptions();  
 
         let minStartDate = 0;
 
@@ -241,6 +402,7 @@ $(document).ready(function () {
         });
 
         $("#add_listing").prop("disabled", true);
+         toggleRemoveButton();
 
     });
 
@@ -248,9 +410,14 @@ $(document).ready(function () {
     $(document).on("click", ".removeCross", function () {
         if ($(".eachListing").length > 1) {
             $(this).closest(".eachListing").remove();
+            updateProfileOptions();  
         }
 
     });
+
+     toggleRemoveButton();
+    checkAllRows();
+    checkDateOverlap();
 
 });
 
@@ -314,9 +481,107 @@ function checkDateOverlap() {
     }
 
 }
+
+function updateProfileOptions() {
+
+    let selectedProfiles = [];
+
+    $('select[name="massage_id[]"]').each(function () {
+        let val = $(this).val();
+        if (val) {
+            selectedProfiles.push(val);
+        }
+    });
+
+    $('select[name="massage_id[]"]').each(function () {
+
+        let current = $(this).val();
+
+        $(this).find('option').each(function () {
+
+            let optionVal = $(this).val();
+
+            if (optionVal !== "" && selectedProfiles.includes(optionVal) && optionVal !== current) {
+                $(this).hide();
+            } else {
+                $(this).show();
+            }
+
+        });
+
+    });
+}
+
+function toggleRemoveButton() {
+    $(".eachListing .removeCross").show();
+    $(".eachListing:first .removeCross").hide();
+   
+}
+
 $(".js_datepicker").datepicker({
     dateFormat: "yy-mm-dd",
     minDate: 0
 });
+
+///////// Proceed To Payment //////////////////////
+
+$(".save_profile_btn").click(function(){
+
+let html = '';
+let total = 0;
+let i = 1;
+
+$(".eachListing").each(function(){
+
+let profile = $(this).find('select[name="massage_id[]"] option:selected').text();
+let start = $(this).find('.profile_start').val();
+let end = $(this).find('.profile_end').val();
+
+let startDate = new Date(start);
+let endDate = new Date(end);
+
+let days = Math.ceil((endDate - startDate) / (1000*60*60*24)) + 1;
+
+let rate = 10; // testing
+let fullFee = rate * days;
+let discount = 0;
+let finalFee = fullFee - discount;
+
+total += finalFee;
+
+html += `
+<tr>
+<td>${i}</td>
+<td>${profile}</td>
+<td>${start}</td>
+<td>${end}</td>
+<td>${days}</td>
+<td>$ ${rate.toFixed(2)}</td>
+<td>$ ${fullFee.toFixed(2)}</td>
+<td>$ ${discount.toFixed(2)}</td>
+<td>$ ${finalFee.toFixed(2)}</td>
+</tr>
+`;
+
+i++;
+
+});
+
+html += `
+<tr>
+<td colspan="7"></td>
+<td><strong>Total Fees</strong></td>
+<td><strong>$ ${total.toFixed(2)}</strong></td>
+</tr>
+`;
+
+$("#summaryBody").html(html);
+$("#summaryModal").css("display","flex").hide().fadeIn();
+});
+
+$(document).on("click",".close-btn",function(){
+$("#summaryModal").hide();
+});
+
 </script>
 @endpush
