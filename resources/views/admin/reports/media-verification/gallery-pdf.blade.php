93860c6daf3e81abb73e18bf4abf500a2d2edf85
Ajay Pandey <ajay.pandey@delimp.com>
2026-03-23 16:15:23 +0530

    Update gallery PDF to use specific variables for banner and pinup images

--- resources/views/admin/reports/media-verification/gallery-pdf.blade.php
+++ resources/views/admin/reports/media-verification/gallery-pdf.blade.php
@@ -62,7 +62,7 @@
                 <td colspan="5">
                     <div><h2 style="margin-top: 0px;font-size: 18px;">Banner Images</h2></div>
                     <div id="bannerImages">     
-                        @if($mediaImages)
+                        @if($bannerImage)
                             @foreach($bannerImage as $banner_image)
                                 {!! $banner_image !!}
                             @endforeach
@@ -78,7 +78,7 @@
                 <td colspan="5">
                     <div><h2 style="margin-top: 0px;font-size: 18px;">Pinup Images</h2></div>
                     <div id="pinupImages">
-                        @if($mediaImages)
+                        @if($pinupImage)
                             @foreach($pinupImage as $pinup_image)
                                 {!! $pinup_image !!}
                             @endforeach
