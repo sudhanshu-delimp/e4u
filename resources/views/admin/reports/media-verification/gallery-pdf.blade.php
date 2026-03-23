483b748c617cc9dc2532f135426e9700b5460397
shiv-delimp <shivendra@delimp.com>
2026-03-23 12:08:14 +0530

    Design verification image gallery PDF.

---
+++ resources/views/admin/reports/media-verification/gallery-pdf.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Image Gallery PDF</title>
+</head>
+<body style="font-family: Arial, sans-serif; margin: 0; padding: 0;">
+
+    <!-- Main Container -->
+    <div style="width: 1000px; margin: 0 auto; padding: 20px;" class="content_body">
+
+        <!-- Print Button -->
+        
+
+        <!-- Hide button during print -->
+        <style>
+            .content_body{
+                 background-color:#ffffff; box-shadow:0px 3px 4px #eeeded
+            }
+            @media print {
+                #printBtn {
+                    display: none;
+                }
+                .content_body{
+                    box-shadow: none;
+                }
+            }
+        </style>
+
+        <table width="100%" cellpadding="10" cellspacing="0" style="border-collapse: collapse;">
+            <thead>
+                <tr>
+                    <td colspan="4">
+                        <h1>Verification Image - E40112</h1>
+                    </td>
+                    <td colspan="1" style="text-align: end">
+                        <div id="printBtn">
+                            <button onclick="window.print()" 
+                                    style="padding: 10px 25px; font-size: 14px; cursor: pointer; background-color:#0c223d;color:#fff; border-radius:5px; font-size:16px; border:none;" >
+                                Print
+                            </button>
+                        </div>
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- Gallery Images -->
+            <tr>
+                <td colspan="5">
+                     
+                    <div>
+                        <div><h2 style="margin-top: 0px;font-size: 18px;">Gallery Images</h2></div>
+                        <img src="https://images.unsplash.com/photo-1773402315969-6f492837d198?q=80&w=1168&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773332585815-f106a5d6ed6c?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773923819983-c594d5d5142b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773061941695-43568f71bee9?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773754371144-9dcb02249c33?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773402315969-6f492837d198?q=80&w=1168&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773332585815-f106a5d6ed6c?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773923819983-c594d5d5142b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773061941695-43568f71bee9?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773754371144-9dcb02249c33?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                    
+                    </div>
+                </td>
+            </tr>
+
+            <!-- Banner Images -->
+            <tr>
+                <td colspan="5">
+                    
+                    <div>
+                        <div><h2 style="margin-top: 0px;font-size: 18px;">Banner Images</h2></div>
+                        <img src="https://images.unsplash.com/photo-1773402315969-6f492837d198?q=80&w=1168&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773332585815-f106a5d6ed6c?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773923819983-c594d5d5142b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773061941695-43568f71bee9?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773754371144-9dcb02249c33?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773402315969-6f492837d198?q=80&w=1168&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773332585815-f106a5d6ed6c?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773923819983-c594d5d5142b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773061941695-43568f71bee9?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773754371144-9dcb02249c33?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                    
+                    </div>
+                </td>
+            </tr>
+           
+
+            <!-- Pinup Images -->
+            <tr>
+            <tr>
+                <td colspan="5">
+                     
+                    <div>
+                        <div><h2 style="margin-top: 0px;font-size: 18px;">Pinup Images</h2></div>
+                        <img src="https://images.unsplash.com/photo-1773402315969-6f492837d198?q=80&w=1168&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773332585815-f106a5d6ed6c?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773923819983-c594d5d5142b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773061941695-43568f71bee9?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                        <img src="https://images.unsplash.com/photo-1773754371144-9dcb02249c33?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"style="width:170px; border: 1px solid #ccc; padding:10px" />
+                    
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+
+        </table>
+
+    </div>
+
+</body>
+</html>
