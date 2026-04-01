f9ff1d748cd4907c144099f9a6964b611916de4d
shiv-delimp <shivendra@delimp.com>
2026-04-01 19:07:33 +0530

    task id 6267263 Public - Footer - Contact Us (Update) WIP

--- resources/views/web/pages/privacy-collection-notice.blade.php
+++ resources/views/web/pages/privacy-collection-notice.blade.php
@@ -1,44 +1,5 @@
 @extends('layouts.web')
 @section('style')
-    <style>
-        .loader {
-            border: 16px solid #f3f3f3;
-            border-radius: 50%;
-            border-top: 16px solid #3498db;
-            width: 120px;
-            height: 120px;
-            -webkit-animation: spin 2s linear infinite;
-            /* Safari */
-            animation: spin 2s linear infinite;
-        }
-
-        /* Safari */
-        @-webkit-keyframes spin {
-            0% {
-                -webkit-transform: rotate(0deg);
-            }
-
-            100% {
-                -webkit-transform: rotate(360deg);
-            }
-        }
-
-        @keyframes spin {
-            0% {
-                transform: rotate(0deg);
-            }
-
-            100% {
-                transform: rotate(360deg);
-            }
-        }
-
-        p,
-        ol,
-        ul {
-            text-align: justify
-        }
-    </style>
 @endsection
 @section('content')
     <section class="padding_top_eight_px padding_bottom_eight_px footer-links-si">
