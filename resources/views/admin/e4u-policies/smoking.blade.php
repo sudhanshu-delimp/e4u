1d7b59e3fd3aaf7ece7fa06a147f2c8514bc5d2a
shiv-delimp <shivendra@delimp.com>
2026-04-03 18:33:17 +0530

    task id 6272900 add a new page smoking

--- resources/views/admin/e4u-policies/smoking.blade.php
+++ resources/views/admin/e4u-policies/smoking.blade.php
@@ -9,7 +9,7 @@
         <div class="container-fluid pl-3 pl-lg-5 pr-3 pr-lg-5">
             <div class="row">
                 <div class="custom-heading-wrapper col-md-12">
-                    <h1 class="h1">Smoking</h1>
+                    <h1 class="h1">Smoke and Vape free workplace policy</h1>
                     <span class="helpNoteLink font-weight-bold" data-toggle="collapse" data-target="#notes"
                         aria-expanded="true">Help?</span>
                 </div>
@@ -17,7 +17,16 @@
                     <div class="card collapse" id="notes" style="">
                         <div class="card-body">
                             <p class="mb-0" style="font-size: 20px;"><b>Notes:</b> </p>
-                            <ol></ol>
+                            <ol>
+                                <li>Blackbox Tech Pty Ltd (ACN 664 919 975) (<b>Blackbox</b>, <b>we</b>, or <b>our</b>).
+                                </li>
+                                <li>We may change or modify this Policy in the future. We will note the date that revisions
+                                    were last made at the bottom of this page. Any revision will take effect upon its
+                                    posting.
+                                    It is your responsibility to check this policy this Policy from time to time to review
+                                    the most
+                                    current version.</li>
+                            </ol>
                         </div>
                     </div>
                 </div>
@@ -25,7 +34,130 @@
             {{-- end --}}
             <div class="row">
                 <div class="col-md-12">
-                    <h3><b>Coming Soon...</b></h3>
+                    <div class="content_wrapper">
+                        <div class="content_details">
+                            <h3><b>1.&nbsp;&nbsp; Overview</b></h3>
+
+
+                            <p>This policy aims to guarantee all employees the right to work in areas free of tobacco and
+                                vape smoke.</p>
+                            <p>
+                                We may change or modify this Policy in the future. We will note the date that revisions
+                                were last made at the bottom of this page. Any revision will take effect upon its posting.
+                                It is your responsibility to check this policy this Policy from time to time to review the
+                                most
+                                current version.
+                            </p>
+                        </div>
+
+                        <div class="content_details">
+                            <h3><b>2.&nbsp;&nbsp; Purpose</b></h3>
+
+
+                            <p>As well as damaging the health of smokers, there is strong scientific evidence that passive
+                                smoking and vaping is hazardous to health.
+                            </p>
+                            <p>
+                                In accordance with our objectives to promote a healthy and safe workplace for those who
+                                work at or visit our premises, Blackbox has developed a smoke and vape free workplace
+                                policy.
+                            </p>
+                        </div>
+
+                        <div class="content_details">
+                            <h3><b>3.&nbsp;&nbsp; Scope</b></h3>
+
+
+                            <p>This policy applies to all Blackbox employees and Agents. Visitors to our premises must also
+                                comply with this policy.
+                            </p>
+                        </div>
+
+                        <div class="content_details">
+                            <h3><b>4.&nbsp;&nbsp; Policy</b></h3>
+
+
+                            <p>To protect and enhance our indoor air quality and to contribute to the health and well-being
+                                of all employees, Agents and visitors to our premises, Blackbox is an entirely smoke and
+                                vape
+                                free workplace. Smoking and vaping is banned from our premises and surrounding area.</p>
+                            <p>
+                                Smoking and vaping is prohibited in all work and other common areas. This includes
+                                conference and meeting rooms, private offices, hallways, entrances, meal rooms, stairs,
+                                restrooms, car parks, employer owned or leased vehicles, and all enclosed facilities.
+                            </p>
+                            <p>
+                                No additional breaks are allowed to any employee who smoke or vape.
+                            </p>
+                        </div>
+
+                        <div class="content_details">
+                            <h3><b>4.1&nbsp;&nbsp; Breach of Policy</b></h3>
+
+
+                            <p>Failure to comply with obligations under this policy may lead to disciplinary action being
+                                taken
+                                by Blackbox. For Blackbox employees and Agents, such disciplinary action may include the
+                                termination of employment or engagement.
+                            </p>
+                        </div>
+
+                        <div class="content_details">
+                            <h3><b>4.2&nbsp;&nbsp; Assistance for smokers</b></h3>
+
+
+                            <p>Blackbox recognises the benefits of a smoke and vape free environment for all employees,
+                                Agents and visitors. Blackbox also recognises the challenges this presents for those who do
+                                either smoke or vape. Blackbox is supportive of employees willing to participate in programs
+                                (e.g. QUIT program) that will assist them to stop or curb smoking.</p>
+                            <p>
+                                Information about QUIT programs are available at <a href="https://www.quit.org.au" class="custom_links_design" target="_blank"> www.quit.org.au</a>.
+                            </p>
+                        </div>
+
+                        <div class="content_details">
+                            <h3><b>5.&nbsp;&nbsp; Roles and Responsibilities</b></h3>
+
+
+                            <p>Employees, Agents, contractors and consultants, are responsible for complying with this
+                                policy.</p>
+                            <p>
+                                Managers are responsible for ensuring compliance with the smoking and vaping policy for
+                                areas and teams that they manage.
+                            </p>
+                        </div>
+
+                        <div class="content_details">
+                            <h3><b>6.&nbsp;&nbsp; Further Information</b></h3>
+
+
+                            <p>Please contact your Manager if you require additional information in relation to this policy.
+                            </p>
+                            <p>
+                                We may change or modify this Policy in the future. We will note the date that revisions
+                                were last made at the bottom of this page. Any revision will take effect upon its posting.
+                                It is your responsibility to check this policy this Policy from time to time to review the
+                                most
+                                current version.
+                            </p>
+                        </div>
+
+                        <div class="content_details">
+                            <hr style="background: #000; height:2px;">
+                            <p> This policy was last updated 02-04-2026.</p>
+                        </div>
+
+                    </div>
+
+
+
+
+
+
+
+
+
+
                 </div>
             </div>
         </div>
