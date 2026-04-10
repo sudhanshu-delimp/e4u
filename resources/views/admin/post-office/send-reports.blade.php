9be32efe1f7b6b9dc05f55f1f6c1d8b082f678fc
shiv-delimp <shivendra@delimp.com>
2026-04-10 17:24:51 +0530

    Refactor email footer in send-reports view to use component for improved maintainability

--- resources/views/admin/post-office/send-reports.blade.php
+++ resources/views/admin/post-office/send-reports.blade.php
@@ -429,35 +429,13 @@
                                 <!-- Footer -->
                                 <table width="100%" cellpadding="0" cellspacing="0"
                                     style="background-color:#0C223D; padding: 15px 30px; line-height: 20px; font-family:Arial, sans-serif; color:#ffffff; font-size:14px; text-align:center;">
+                                    {{-- footer from component --}}
                                     <tr>
-                                        <td style="text-align:center;border:0px !important;">
-                                            This is an automatically generated email by the Escorts4U Operations Centre.<br>
-                                            &copy; Copyright 2024 Blackbox Tech Pty Ltd. All rights reserved.
+                                        <td style="padding: 0px;">
+                                                 <x-email-footer />
                                         </td>
                                     </tr>
-
-
-                                    <tr>
-                                        <td style="font-style: italic; text-align:justify; ">
-                                            <p style="font-size: 13px; margin-bottom: 5px;">This email and any attachments
-                                                are confidential between Blackbox Tech Pty Ltd, and the addressee. If it has
-                                                been sent to you in error, please immediately delete it and notify us.</p>
-                                            <p style="font-size: 13px;  margin-bottom: 5px;">Unless expressly stated by the
-                                                parties, this email does not constitute a binding contract.</p>
-                                            <p
-                                                style="font-size: 13px; margin-bottom: 5px;">
-                                                You rely on the information contained in this email at your own risk. We
-                                                accept no liability, and you waive and release us from any liability and for
-                                                any damage caused by this email or its attachments due to viruses,
-                                                interference, interception, corruption or unauthorised access.</p>
-                                            <p
-                                                style="font-size: 13px; margin-bottom: 5px;">
-                                                Personal information contained in emails sent to us is subject to our
-                                                Privacy Policy, available on our website.</p>
-
-                                        </td>
-
-                                    </tr>
+                                    {{-- end --}}
                                 </table>
 
                             </td>
