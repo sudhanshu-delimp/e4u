afa5a680b59e14aacc50827e11d26f649235ef58
shiv-delimp <shivendra@delimp.com>
2026-04-17 12:41:26 +0530

    fixed ui in email templates

--- resources/views/admin/reports/communication/communications.blade.php
+++ resources/views/admin/reports/communication/communications.blade.php
@@ -8,8 +8,13 @@
         .modal-body table td h4{
             font-weight: 700 !important;
         }
-        p.copyright {
+        /* p.copyright {
             padding-top: 15px !important;
+            padding-top: 15px !important;
+        } */
+        .copy_notes{
+            padding-left: 10px !important; 
+            padding-right: 10px !important; 
         }
 
     @media print {
--- resources/views/components/email-footer.blade.php
+++ resources/views/components/email-footer.blade.php
@@ -1,9 +1,9 @@
 <div>
-    <p style="text-align: center; font-size:14px; border-bottom:1px solid #fff; padding-bottom:15px;" class="copyright">
+    <p style="text-align: center; font-size:14px; border-bottom:1px solid #fff; padding-bottom:15px; padding-top:15px;" class="copyright">
         This is an automatically generated email by the Escorts4U Operations Centre.<br>
         © Copyright {{ date('Y') }} Blackbox Tech Pty Ltd. All rights reserved.
     </p>
-    <div class="copy_notes" style="padding-left: 10px; padding-right:10px;">
+    <div class="copy_notes" style="padding-left: 20px; padding-right:20px; text-align:justify;">
         <p style="font-size: 13px; margin-bottom: 10px; font-style: italic; text-align:justify;">This email and any attachments
             are confidential between Blackbox Tech Pty Ltd, and the addressee. If it has
             been sent to you in error, please immediately delete it and notify us.</p>
