cab340c301af0596b4b96a486a0288bf02d8c55f
Mithilesh Kumar <mithilesh@delimp.com>
2026-04-29 15:33:48 +0530

    Model added into shareholder

--- resources/views/layouts/shareholder.blade.php
+++ resources/views/layouts/shareholder.blade.php
@@ -19,7 +19,31 @@
             </div>
             {{-- end main content --}}
         </div>
-
+         <div class="modal upload-modal fade" id="comman_modal" style="display: none">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content ">
+                    <div class="modal-header">
+                        <h5 class="modal-title"><img src="{{ asset('assets/dashboard/img/upload-photos.png') }}" class="custompopicon"></h5>
+                        <span class="head_modal_title" style="color:#fff;"></span>
+                        
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">
+                        <img src="{{ asset('assets/app/img/newcross.png')}}" class="img-fluid img_resize_in_smscreen">
+                        </span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <h1 class="my-4 custom_modal_text" style="text-align: center;">
+                        <span id="comman_str"></span>
+                        <span class="comman_msg"></span>
+                        </h1>
+                    </div>
+                    <div class="modal-footer" class="justify-content-center pt-0">
+                        <button type="submit" class="btn-success-modal" data-dismiss="modal" id="close">Ok</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     
