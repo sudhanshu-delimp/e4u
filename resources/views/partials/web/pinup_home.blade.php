2d04d9b1aeedd0e9bc597e7cddeb318c8d92be68
shiv-delimp <shivendra@delimp.com>
2026-05-11 18:56:24 +0530

    Adds media pending icon to pinup profile views
    Enhances user interface by displaying a "Media Pending" verification icon on pinup profile sections and updates modal and styling for improved consistency. Improves user visibility into media approval status and aligns component layouts for better clarity.

--- resources/views/partials/web/pinup_home.blade.php
+++ resources/views/partials/web/pinup_home.blade.php
@@ -1,4 +1,4 @@
-<div href="#" class="tip mb-2 d_custom_home_img">
+<div href="#" class="tip mb-2 d_custom_home_img lg_icon_wrapper">
     <img style="" class="img-fluid"
         src="{{ !empty($escort->user->defaultPinupImage) ? asset($escort->user->defaultPinupImage->path) : asset('assets/app/img/home/home-demo.png') }}">
     <span class="memmber_info"><i class="fa fa-user"></i> Member ID: {{ $escort->user->member_id }}</span>
@@ -11,4 +11,11 @@
         <div class="trikon_style manage_toolkit_font"><a href="{{ route('web.pinup', $escort->id) }}">I am your Pin Up
                 click here.</a></div>
     @endif
+    
+    <div class="lg_verify_icon">
+                                
+        <img src="{{ ('assets/app/img/pending_icon/e4u_pending_REV.png') }}">
+        <span class="common_shield_tooltip">Media Pending</span>
+
+    </div>
 </div>
