d9874ee41e9cfd355d741dd06e78a177ebc5f6e9
raj_tiwari <raj.tiwari@delimp.com>
2026-06-05 16:46:33 +0530

    [TASK-5545060] Increase maximum file upload size limit to 10MB across multiple upload-avatar views

--- resources/views/center/my-account/upload-avatar.blade.php
+++ resources/views/center/my-account/upload-avatar.blade.php
@@ -499,7 +499,7 @@
             oversize = getBase64SizeBytes(src) > maxBytes;
         }
         if (oversize) {
-            $('.comman_msg').text('Image must be 2MB or less.');
+            $('.comman_msg').text('Image must be 10MB or less.');
             $("#comman_modal").modal('show');
             try { removeUpload(); } catch (e) {}
             return false;
--- resources/views/shareholder/dashboard/my-account/upload-my-avatar.blade.php
+++ resources/views/shareholder/dashboard/my-account/upload-my-avatar.blade.php
@@ -300,7 +300,7 @@
 
 
             // Client-side 2MB check before sending AJAX
-            var maxBytes = 2 * 1024 * 1024;
+            var maxBytes = 10 * 1024 * 1024;
             var inputEl = $('.file-upload-input')[0];
             var oversize = false;
             if (inputEl && inputEl.files && inputEl.files[0]) {
@@ -309,7 +309,7 @@
                 oversize = getBase64SizeBytes(src) > maxBytes;
             }
             if (oversize) {
-                $('.comman_msg').text('Image must be 2MB or less.');
+                $('.comman_msg').text('Image must be 10MB or less.');
                 $("#comman_modal").modal('show');
                 try {
                     removeUpload();
