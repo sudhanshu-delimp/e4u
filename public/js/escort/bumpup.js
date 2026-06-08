7d7839d2eb6b7cf66aa4a813fd6579f1ec3c296c
Sudhanshu Bisht <sudhanshu@delimp.com>
2026-06-08 17:09:38 +0530

    6447764, all option form buttons handling accroding to their work scope

--- public/js/escort/bumpup.js
+++ public/js/escort/bumpup.js
@@ -1,6 +1,6 @@
 let bumpUpFrom = $(".modal-form-bumpUp form");
 let bumpUpFromFromButton = bumpUpFrom.find('.modal-footer button[type="button"]');
-console.log('bumpUpFrom', bumpUpFromFromButton);
+
 bumpUpFromFromButton.prop('disabled', true);
 $("#modalPaymentButton").prop("disabled", true);
 $(document).on('change', '#bumpUpProfileId', function () {
@@ -50,60 +50,3 @@ bumpUpFrom.on('submit', function (e) {
         }
     });
 });
-
-// bumpUpFromFromButton.addEventListener("click", function (e) {
-//     e.preventDefault();
-
-//     const button = e.target
-//     const form = button.closest('form');
-//     if (!form) {
-//         console.error("Form not found!");
-//         return;
-//     }
-//     const action = form.action;
-//     const method = form.method;
-//     const formData = new FormData(form);
-//     $.ajax({
-//         url: action,
-//         method: method,
-//         data: formData,
-//         processData: false,
-//         contentType: false,
-//         headers: {
-//             'Accept': 'application/json',
-//             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
-//         },
-//         beforeSend: function () {
-//             bumpUpFromFromButton.prop('disabled', true);
-//         },
-//         success: function (data) {
-//             if (data.success) {
-//                 Swal.fire({
-//                     icon: 'success',
-//                     text: data.message
-//                 });
-//                 form.reset();
-//                 $("#bumpup_profile").modal('hide');
-//                 table.draw();
-//             }
-//         },
-//         error: function (xhr) {
-//             if (xhr.status === 422) {
-//                 let messages = Object.values(JSON.parse(xhr.responseText).errors).flat().join('<br>');
-//                 Swal.fire({
-//                     icon: 'error',
-//                     title: 'Validation Error',
-//                     html: messages
-//                 });
-//             } else {
-//                 let message = JSON.parse(xhr.responseText).message;
-//                 Swal.fire({
-//                     icon: 'error',
-//                     title: xhr.statusText,
-//                     text: message || 'Something went wrong.'
-//                 });
-//             }
-//             savePinupButton.disabled = false;
-//         }
-//     });
-// });
