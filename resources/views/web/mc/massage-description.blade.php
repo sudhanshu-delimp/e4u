101d9d62443be15a8c29e745fa2e4b1864c2db77
shiv-delimp <shivendra@delimp.com>
2026-06-11 17:32:31 +0530

    Task ID 6496069 working in progress for responsive public

--- resources/views/web/mc/massage-description.blade.php
+++ resources/views/web/mc/massage-description.blade.php
@@ -826,7 +826,7 @@ margin-right: 5px;
 
                                                                 <img src="{{ $image['url'] }}"
                                                                     class="img-responsive"
-                                                                    style="width: 305px;height: 374px;object-fit: cover;border-radius:10px;">
+                                                                    >
                                                                 <div class="hover-overlay">
                                                                     <span>Click me!</span>
                                                                 </div>
