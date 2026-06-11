c1acbc8f24610f81cbe76eccefab95295f9235af
raj_tiwari <raj.tiwari@delimp.com>
2026-06-11 17:25:43 +0530

    [TASK-4177054] Enhance Fees Summary and Communication features with improved email filtering and logging

--- app/Http/Controllers/Agent/FeesSummeryController.php
+++ app/Http/Controllers/Agent/FeesSummeryController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers\Agent;
 
 use App\Http\Controllers\Controller;
+use App\Models\PaymentHistory;
+use App\Models\PaymentItem;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FeesSummeryController extends Controller
 {
     public function feesSummery(){
+
+      //get all appointend Advertiser 
+      $user = User::whereIn('type', ['3', '4'])->where('assigned_agent_id', Auth::id())->first();
+     // dd($user);
+     $data = PaymentItem::with(['item', 'payment' => function($query) use ($user) {
+      $query->user_id = $user->id;
+     }])->where('id', 10)->first();
+     //dd($data);
+     
+    // dd($data->item()->first());
+
+     //PaymentHistory
+     $phitry =  PaymentHistory::get();
+     //dd($phitry);
+     //dd($data->item()->get(), $data->payment()->first());
       return  view('agent.dashboard.Fees.summary');
     }
 }
