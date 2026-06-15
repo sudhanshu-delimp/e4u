c855cc0745bac416ab397871b595c94654f65be0
raj_tiwari <raj.tiwari@delimp.com>
2026-06-15 12:48:40 +0530

    [TASK-4177054] Add member_id to EmailLog model and update LogSentEmail listener for email logging.

--- app/Http/Controllers/Agent/FeesSummeryController.php
+++ app/Http/Controllers/Agent/FeesSummeryController.php
@@ -14,17 +14,27 @@ class FeesSummeryController extends Controller
     public function feesSummery(){
 
       //get all appointend Advertiser 
-      $user = User::whereIn('type', ['3', '4'])->where('assigned_agent_id', Auth::id())->first();
-     // dd($user);
+       $user = User::whereIn('type', ['3', '4'])->where('assigned_agent_id', Auth::id())->first();
+
+      //dd($query->first());
      $data = PaymentItem::with(['item', 'payment' => function($query) use ($user) {
       $query->user_id = $user->id;
-     }])->where('id', 10)->first();
-     //dd($data);
+     }])->first();
+     dd($data);
+
+      $d = PaymentHistory::where('user_id', $user->id)->with(['items' => function($query){
+        $query->whereHas('item', function($q){
+          //$q->where('membership', 2);
+        });
+      }])->first();
+      dd($d);
+
+     dd($d->items()->first()->item()->first()->membership == 2);
      
     // dd($data->item()->first());
 
      //PaymentHistory
-     $phitry =  PaymentHistory::get();
+    //  $phitry =  PaymentHistory::get();
      //dd($phitry);
      //dd($data->item()->get(), $data->payment()->first());
       return  view('agent.dashboard.Fees.summary');
