446b41f3bc14187137605e7d98e3aada6278c645
raj_tiwari <raj.tiwari@delimp.com>
2026-06-15 13:45:23 +0530

    [TASK-4177054] update code

--- app/Http/Controllers/Agent/FeesSummeryController.php
+++ app/Http/Controllers/Agent/FeesSummeryController.php
@@ -13,30 +13,6 @@ class FeesSummeryController extends Controller
 {
     public function feesSummery(){
 
-      //get all appointend Advertiser 
-      //  $user = User::whereIn('type', ['3', '4'])->where('assigned_agent_id', Auth::id())->first();
-
-      //dd($query->first());
-    //  $data = PaymentItem::with(['item', 'payment' => function($query) use ($user) {
-    //   $query->user_id = $user->id;
-    //  }])->first();
-    //  dd($data);
-
-      // $d = PaymentHistory::where('user_id', $user->id)->with(['items' => function($query){
-      //   $query->whereHas('item', function($q){
-      //     //$q->where('membership', 2);
-      //   });
-      // }])->first();
-      // dd($d);
-
-    //  dd($d->items()->first()->item()->first()->membership == 2);
-     
-    // dd($data->item()->first());
-
-     //PaymentHistory
-    //  $phitry =  PaymentHistory::get();
-     //dd($phitry);
-     //dd($data->item()->get(), $data->payment()->first());
       return  view('agent.dashboard.Fees.summary');
     }
 }
