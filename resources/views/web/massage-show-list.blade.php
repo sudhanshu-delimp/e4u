e57c8b209942415df804fd6d0a1d4f3768990647
shiv-delimp <shivendra@delimp.com>
2026-07-01 12:09:00 +0530

    align Newzeland location in footer and update icons of back to listing in shortlist page EC and MC

--- resources/views/web/massage-show-list.blade.php
+++ resources/views/web/massage-show-list.blade.php
@@ -317,9 +317,8 @@
                                 <div class="display_inline_block mb-1 mr-2 ">
                                     <a type="submit" href="#" class="btn reset_filter p-1"
                                         data-toggle="tooltip">
-                                        {{-- <i class="fa fa-back" aria-hidden="true"></i> --}}
-                                        <i class="fa fa-arrow-left ml-0" aria-hidden="true"
-                                            style="padding: 5px;font-size: 16px;"></i>
+                                       <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <g opacity="0.4"> <path d="M9.00039 15.3802H13.9204C15.6204 15.3802 17.0004 14.0002 17.0004 12.3002C17.0004 10.6002 15.6204 9.22021 13.9204 9.22021H7.15039" stroke="#ffffff" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M8.57 10.7701L7 9.19012L8.57 7.62012" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
+                
                                         Back To Listings
                                     </a>
                                 </div>
--- resources/views/web/mc-shortlist/massage-centre-list.blade.php
+++ resources/views/web/mc-shortlist/massage-centre-list.blade.php
@@ -137,7 +137,8 @@
                         <button class="back_to_list_btn">
                         
                             <a type="submit" href="./massage-centres-list" data-toggle="tooltip">                                
-                                <i class="fa fa-arrow-left ml-0" aria-hidden="true" style="padding: 5px;font-size: 16px;"></i>
+                                <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <g opacity="0.4"> <path d="M9.00039 15.3802H13.9204C15.6204 15.3802 17.0004 14.0002 17.0004 12.3002C17.0004 10.6002 15.6204 9.22021 13.9204 9.22021H7.15039" stroke="#ffffff" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M8.57 10.7701L7 9.19012L8.57 7.62012" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
+                
                                 <span class="hide-on-sm" style="margin-right: 10px;"> Back To Listings</span>
                             </a>
                         </button>
