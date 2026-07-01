bf5e557b30b380b28799492058aeab20b8e5205e
Ashish Kumar <ashish.kumar@delimp.com>
2026-07-01 12:45:09 +0530

    display delivery type uc first in order lists

--- resources/views/admin/Concierge/product-request.blade.php
+++ resources/views/admin/Concierge/product-request.blade.php
@@ -70,13 +70,13 @@
 
                                 </tbody>
                                 <tr>
-                                    <th colspan="9" class="border-0"></th>
+                                    <th colspan="10" class="border-0"></th>
                                 </tr>
                                 <tfoot class="bg-first t-foot">
                                     <tr>
                                         <th colspan="3" class="text-left border-0">Server time: <span
                                                 class="serverTime">{{ date('d-m-Y h:i a') }}</span></th>
-                                        <th colspan="3" class="text-center border-0">Refresh time:<span
+                                        <th colspan="4" class="text-center border-0">Refresh time:<span
                                                 class="refreshSeconds"> 15</span></th>
                                         <th colspan="3" class="text-right border-0" style="text-align: right!important;">
                                             Up time: <span class="uptimeClass">{{ getAppUptime() }}</span></th>
@@ -311,7 +311,10 @@
                     // },
                     {
                         data: 'delivery_type',
-                        name: 'delivery_type'
+                        name: 'delivery_type',
+                        render: function(data) {
+                            return data ? data.charAt(0).toUpperCase() + data.slice(1) : '';
+                        }
                     },
                     // {
                     //     data: 'gst_amount',
