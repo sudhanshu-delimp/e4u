d85484770635f52cc3954a8bee9bf35e24996416
Mithilesh Kumar <mithilesh@delimp.com>
2026-07-01 16:14:34 +0530

    4491404:Added review list into massge dashoard

--- resources/views/center/dashboard/Reviews/view-reviews.blade.php
+++ resources/views/center/dashboard/Reviews/view-reviews.blade.php
@@ -1,5 +1,5 @@
 
-@extends('layouts.escort')
+@extends('layouts.center')
 @section('style')
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/select2/select2.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/toast-plugin/jquery.toast.min.css') }}">
