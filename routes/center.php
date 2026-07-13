b79afc40be48b1fce7a2a952904a0effaa7c46ec
Sudhanshu Bisht <sudhanshu@delimp.com>
2026-07-13 16:01:17 +0530

    4391243

--- routes/center.php
+++ routes/center.php
@@ -212,8 +212,8 @@ Route::get('payments/{payment}/print', [PaymentController::class, 'printPaymentD
 # Wallet Module
 Route::get('my-wallet', [WalletController::class, 'index'])->name('center.my_wallet');
 Route::get('wallet_transaction', [WalletController::class, 'transactionList'])->name('center.wallet_transaction');
-Route::post('advertiser/payments/adjustment', [EscortPaymentController::class, 'paymentAdjustment'])->name('payment.adjustment');
-Route::post('advertiser/payments/process', [EscortPaymentController::class, 'processPayment'])->name('escort.payment.process');
+Route::post('advertiser/payments/adjustment', [EscortPaymentController::class, 'paymentAdjustment'])->name('advertiser.payment.adjustment');
+Route::post('advertiser/payments/process', [EscortPaymentController::class, 'processPayment'])->name('advertiser.payment.process');
 
 
 
