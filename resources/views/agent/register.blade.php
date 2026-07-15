ba24d0852bbe14153fe7b7c2023c37d3daeda9cb
shiv-delimp <shivendra@delimp.com>
2026-07-15 12:53:56 +0530

    Feedback: change ui of changes policy in e4u verify page public

--- resources/views/agent/register.blade.php
+++ resources/views/agent/register.blade.php
@@ -274,7 +274,7 @@
                         <div class="accodien_manage_padding_content">
                             <div class="border_top_one_px padding_ten_px_top_btm">
                                 <div class="row">
-                                    <div class="col-md-4 col-12">
+                                    <div class="col-lg-4 col-md-6 col-sm-12">
                                         <ul
                                             class="padding_zero_px_ul_ol list_style_none font_size_forteenpx mb-0 register_ul">
                                             <li><span class="correct_symbole_font_weight">✓</span> At least 1 lowercase
@@ -282,7 +282,7 @@
                                             <li><span class="correct_symbole_font_weight">✓</span> At least 1 number</li>
                                         </ul>
                                     </div>
-                                    <div class="col-md-4 col-12">
+                                    <div class="col-lg-4 col-md-6 col-sm-12">
                                         <ul
                                             class="padding_zero_px_ul_ol list_style_none font_size_forteenpx mb-0 register_ul">
                                             <li><span class="correct_symbole_font_weight">✓</span> At least 1 uppercase
@@ -292,7 +292,7 @@
 
                                         </ul>
                                     </div>
-                                    <div class="col-md-4 col-12">
+                                    <div class="col-lg-4 col-md-6 col-sm-12">
                                         <ul class="padding_zero_px_ul_ol list_style_none font_size_forteenpx register_ul">
                                             <li><span class="correct_symbole_font_weight">✓</span> 8 characters minimum
                                             </li>
--- resources/views/partials/web/navigation3.blade.php
+++ resources/views/partials/web/navigation3.blade.php
@@ -24,7 +24,7 @@
                      <a class="nav-link" href="{{ route('home') }}">Home</a>
                   </li>
                   <li class="nav-item dropdown">
-                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Find Escort</a>
+                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Find Escorts</a>
                     <div class="dropdown-menu addMyLocation" aria-labelledby="navbarDropdown">
 
                         <a class="dropdown-item" href="{{ route('find.all',['gender'=>6]) }}?lat"><img src="{{asset('assets/app/img/woman-avatar.svg')}}">Female</a>
@@ -36,7 +36,7 @@
                     </div>
                   </li>
                   <li class="nav-item">
-                     <a class="nav-link" href="{{ route('find.massage.centre') }}">Find Massage Centre</a>
+                     <a class="nav-link" href="{{ route('find.massage.centre') }}">Find Massage Centres</a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="javascript:void(0);" data-toggle="modal" data-target="#myPlayboxModal">
