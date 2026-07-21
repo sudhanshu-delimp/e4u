5c3d53feb728f9ba67e050bc499c4537302b0a7c
shiv-delimp <shivendra@delimp.com>
2026-07-21 18:03:48 +0530

    Feedback add icon on MC list and grid video icon

--- resources/views/web/mc-shortlist/mc-grid-data.blade.php
+++ resources/views/web/mc-shortlist/mc-grid-data.blade.php
@@ -65,6 +65,14 @@
                 <div class="mc_card_content">
                     <div class="items">
                         <span class="title text_truncate">{{  get_massage_home_city($listing->user_id) }}</span>
+                        <span class="decs">
+                            @php
+                               $videoCnt = checkVideoExistInMcProfile($listing->user_id);
+                            @endphp
+                            @if($videoCnt > '0')
+                                <img src="{{ asset('assets/app/img/video_play.svg') }}">                           
+                            @endif
+                        </span>
                           <span class="mc_star">
                             @for($i=1; $i<= 5; $i++)
                                 @if($listing->star_rating && $listing->star_rating > 0 && $i <= $listing->star_rating)
@@ -76,12 +84,7 @@
                         </span>
                     </div>
                     
-                    <div class="items">
-                        <span class="title">Video: </span>
-                        <span class="decs">
-                            <img src="{{ asset('assets/app/img/video_play.svg') }}">
-                        </span>
-                    </div>
+                   
                     <div class="items">
                         <span class="title">Hours: </span>
                         <span class="decs">{{get_working_hours($listing)}}</span>
