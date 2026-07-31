658f4b0b75e5d07b9119105775ab5b1b06f3be9a
Mithilesh Kumar <mithilesh@delimp.com>
2026-07-31 17:05:44 +0530

    4177053: Worked on agent fee query view page

--- resources/views/agent/dashboard/Fees/view_query.blade.php
+++ resources/views/agent/dashboard/Fees/view_query.blade.php
@@ -1,6 +1,4 @@
-@php
-use Illuminate\Support\Carbon;
-@endphp
+
 @if ($queryData->count() > 0)
     <table class="table table-bordered mb-0">
         <thead class="table-bg modal-thaed">
@@ -12,7 +10,7 @@ use Illuminate\Support\Carbon;
              @foreach ($queryData as $query)
             <tr>
                 <td>{{$query->notes}}</td>
-                <td>{{Carbon::parse($query->report_date)->format('d-m-Y')}}</td>
+                <td>{{basicDateFormat($query->report_date)}}</td>
             </tr>
             @endforeach
        
