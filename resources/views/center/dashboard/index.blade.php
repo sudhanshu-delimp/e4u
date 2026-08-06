ab92b933ac023b11f1274629eef873d7517fb061
shiv-delimp <shivendra@delimp.com>
2026-08-06 17:52:35 +0530

    feednack: change route My Wallet to Our Wallet

--- resources/views/center/dashboard/index.blade.php
+++ resources/views/center/dashboard/index.blade.php
@@ -207,7 +207,7 @@
                         <img src="{{ asset('assets/dashboard/img/boxicon/wallet.png') }}" alt="My Wallet">
                     </div>
                     <h2>
-                        My Wallet
+                        Our Wallet
                     </h2>
                 </a>
 
