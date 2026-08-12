8bc1db52fff0cb941d6b7080f86bba7b9b078013
Mithilesh Kumar <mithilesh@delimp.com>
2026-08-12 12:53:00 +0530

    Fixed the next previous issues into massage detail page

--- app/Http/Controllers/MassageCentre.php
+++ app/Http/Controllers/MassageCentre.php
@@ -635,8 +635,8 @@ class MassageCentre extends Controller
         if (str_contains($previousSlug, 'massage-profile') || str_contains($previousSlug, 'massage-centres-list')) {
             $relatedMassges = MassagePurchase::with('massageprofile')->where('status', 'listed')
                 ->whereHas('user', function ($q) use($stateId) {
-                    $q->where('status', 1)
-                    ->where('state_id', $stateId);
+                    $q->where('status', 1);
+                    //->where('state_id', $stateId);
                 })
                 ->whereNotIn('massage_profile_id', $blockedProfileForViewersIds)
                 ->whereDoesntHave('activeSuspendProfile')->get();
