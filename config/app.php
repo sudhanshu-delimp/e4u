d3886629e0b3ad1ce4d3531c8743c2c449497ecb
Ashish Kumar <ashish.kumar@delimp.com>
2026-08-18 11:08:37 +0530

    pagination length and range set for agent and viewver console

--- config/app.php
+++ config/app.php
@@ -252,7 +252,7 @@ return [
 
   
   'loyalty_amount' => (float) env('LOYALTY_AMOUNT', 200),
-  'paginate_range' =>  env('PAGINATE_RANGE'),
+  'paginate_range' =>  env('PAGINATE_RANGE',"10, 25, 50, 75, 100"),
   'paginate_length' =>  env('PAGINATE_LENGTH',25),
 
 ];
