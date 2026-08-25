32f9c52ed2f947d5e8ec47f9c8680cdbd42e6cc3
Ashish Kumar <ashish.kumar@delimp.com>
2026-08-25 15:08:07 +0530

    display first and last name

--- resources/views/escort/dashboard/Concierge/visa-migration.blade.php
+++ resources/views/escort/dashboard/Concierge/visa-migration.blade.php
@@ -99,27 +99,22 @@
                             <b>Your details:</b>
                             <div class="mt-2">
 
-                                {{-- First Name --}}
-                                {{-- <div class="form-group">
-                                    <label for="business_name" class="required"><b>Business Name</b></label>
-                                    <input id="business_name" placeholder="Business Name" name="business_name"
-                                        type="text" class="form-control">
-
-                                    <span class="text-danger error-text business_name_error"></span>
-                                </div> --}}
-
-                                {{-- Last Name --}}
+                                @php
+                                    $nameParts = preg_split('/\s+/', trim(Auth::user()->name));
+                                    $firstName = $nameParts[0] ?? '';
+                                    $lastName = count($nameParts) > 1 ? implode(' ', array_slice($nameParts, 1)) : '';
+                                @endphp
                                 <div class="form-group">
                                     <label for="first_name"><b>First Name</b></label>
-                                    <input id="first_name" placeholder="First Name" name="first_name" type="text"
-                                        class="form-control">
+                                    <input id="first_name" placeholder="First Name" name="first_name"
+                                        value="{{ $firstName }}" type="text" class="form-control">
 
                                     <span class="text-danger error-text first_name_error"></span>
                                 </div>
                                 <div class="form-group">
                                     <label for="last_name"><b>Last Name</b></label>
-                                    <input id="last_name" placeholder="Last Name" name="last_name" type="text"
-                                        class="form-control">
+                                    <input id="last_name" placeholder="Last Name" name="last_name"
+                                        value="{{ $lastName }}" type="text" class="form-control">
 
                                     <span class="text-danger error-text last_name_error"></span>
                                 </div>
@@ -320,8 +315,7 @@
                         Visa Services - Request Confirmation
                     </h5>
                     <button type="button" class="close text-white" data-dismiss="modal">
-                        <img src="{{asset('assets/app/img/newcross.png')}}"
-                            class="img-fluid img_resize_in_smscreen">
+                        <img src="{{ asset('assets/app/img/newcross.png') }}" class="img-fluid img_resize_in_smscreen">
                     </button>
                 </div>
                 <div class="modal-body">
@@ -506,7 +500,7 @@
             e.preventDefault();
             let form = this;
             let submitButton = $("#submitAssistanceRequest");
-                      
+
             // Clear previous errors
             $(".error-text").text("");
             submitButton.prop("disabled", true).text("Sending...");
