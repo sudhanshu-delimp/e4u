cdbf458d694779429505e2f8e0d97b3257970d59
Mithilesh Kumar <mithilesh@delimp.com>
2026-09-05 23:22:51 +0530

    Fixed the pdf spacing issues

--- resources/views/admin/management/operator/fees/print_monthly_report.blade.php
+++ resources/views/admin/management/operator/fees/print_monthly_report.blade.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Operator Montly Fee Report</title>
-    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"> --}}
-    {{-- <link href="{{ asset('assets/dashboard/css/dk-style.css?v1.2') }}" rel="stylesheet">
- --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <link href="{{ asset('assets/dashboard/css/dk-style.css?v1.2') }}" rel="stylesheet">
+
     <style>
         .heading {
             display: block;
@@ -37,7 +37,6 @@
 
         .table td {
             vertical-align: middle;
-            border: none !important
         }
 
         table td {
