9f1caf1159f6fef11508a6d3a1737e61feac658f
Mithilesh Kumar <mithilesh@delimp.com>
2026-09-15 11:28:39 +0530

    Added profile id into profile list page

--- app/Repositories/Purchase/PurchaseRepository.php
+++ app/Repositories/Purchase/PurchaseRepository.php
@@ -45,9 +45,12 @@ class PurchaseRepository extends BaseRepository implements PurchaseInterface
     public function paginatedList($start, $limit, $order_key, $dir, $columns, $search = null, $user_id, $conditions = [], $conditionsIn = [])
     {
         // $order_field = $this->getOrderPurchase($order_key, $conditionsIn);
+       
 
         $order_field = $columns[$order_key]['name'];
         $searchables = $this->getSearchableFields($columns);
+        $searchables[] = 'slug';
+        
         $table = $this->model->getTable();
         $query = $this->model
             ->where($conditions)
@@ -68,6 +71,7 @@ class PurchaseRepository extends BaseRepository implements PurchaseInterface
                     if (count($conditionsIn) > 0 && isset($conditionsIn['column']) && isset($conditionsIn['condition'])) {
                         $sub_query->where(function ($q) use ($search) {
                             $q->where('profile_name', 'LIKE', "%{$search}%")
+                             ->orWhere('slug', 'LIKE', "%{$search}%")
                                 ->orWhereHas('user', function ($q) use ($search) {
                                     $q->where('member_id', 'LIKE', "%{$search}%");
                                 });
@@ -145,11 +149,13 @@ class PurchaseRepository extends BaseRepository implements PurchaseInterface
             $isExtended = $item->isListingExtended();
             $mainPurchase = $item->escort->mainPurchase;
             $isBumpUped = $item->escort->activeBumpup;
+            $slug = isset($item->escort->slug) ? $item->escort->slug : $item->escort_id;
 
             $item->is_bumpup = !empty($isBumpUped) ? true : false;
             $item->statusOriginal = $item->status;
-
-            $item->escort_id = $item->escort_id;
+            $item->slug = $item->escort->slug;
+            //$item->escort_id = $item->escort_id;
+            $item->escort_id = $slug;
             $item->member_id = $item->escort->member_id;
             $item->name = $item->escort->user->name;
             $item->profile_name = "<span> {$item->escort->profile_name}";
