da9cee4f17eeee04cc7c36dc1e682d6c9928a7e3
Ajay Pandey <ajay.pandey@delimp.com>
2026-09-25 15:11:06 +0530

    Rename My Statistics to Advertiser Statistics and update related routes and sidebar links

--- routes/agent.php
+++ routes/agent.php
@@ -239,7 +239,7 @@ Route::get('forms',function(){
     // })->name('Fees.summary');
 
 
-Route::get('my-statistics',[AgentController::class,'myStatistics'])->name('agent.my-statistics');
+Route::get('advertiser-statistics',[AgentController::class,'myStatistics'])->name('agent.my-statistics');
 
 Route::get('advertisers',function(){
     return view('agent.dashboard.advertisers');
